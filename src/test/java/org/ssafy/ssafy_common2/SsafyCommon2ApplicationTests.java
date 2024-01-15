a06b72a12634b61697a38e8e01fe282562b7dc60
SSAFY <osy9536@naver.com>
2024-01-15 11:25:06 +0900

    Init : 초기 카카오 소셜 로그인 구현

---
+++ src/test/java/org/ssafy/ssafy_common2/SsafyCommon2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ssafy.ssafy_common2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafyCommon2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
