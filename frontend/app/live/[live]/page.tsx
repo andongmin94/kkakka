46dc6ac2e48f05dd281eb01eb54c1383481ce0c0
lhgeer2617 <lhgeer2617@gmail.com>
2024-01-15 13:45:38 +0900

    Feat : #13 디렉토리 구성

---
+++ frontend/app/live/[live]/page.tsx
@@ -0,0 +1,7 @@
+export default function LivePage() {
+  return (
+    <>
+      <h1>LivePage</h1>
+    </>
+  );
+}
---
+++ frontend/app/profile/[profile]/page.tsx
@@ -0,0 +1,7 @@
+export default function ProfilePage() {
+  return (
+    <>
+      <h1>ProfilePage</h1>
+    </>
+  );
+}
