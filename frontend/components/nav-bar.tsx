083445abb8f61bdcc44fe6415e7a4a9c4c8dc32f
김상훈 <k1016h@naver.com>
2024-01-15 17:01:30 +0900

    Feat : 네브바 사이드바 추가 (1/2)
    폴리싱이 필요하나 시간 관계상 일단 커밋합니다.

---
+++ frontend/components/nav-bar.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+import { Menu } from "lucide-react"
+import Image from "next/image"
+
+export default function Navbar() {
+  const [state, setState] = React.useState(false)
+
+  const menus = [
+    { title: "Home", path: "/your-path" },
+    { title: "Blog", path: "/your-path" },
+    { title: "About Us", path: "/your-path" },
+    { title: "Contact Us", path: "/your-path" },
+  ]
+
+  return (
+    <nav className="bg-white w-full border-b md:border-0">
+      <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
+        <div className="flex items-center justify-between py-3 md:py-5 md:block">
+          <Link href="/">
+          <Image src="/logo.svg" alt="logo" width={150} height={130} priority />
+          </Link>
+          <div className="md:hidden">
+            <button
+              className="text-gray-700 outline-none p-2 rounded-md focus:border-gray-400 focus:border"
+              onClick={() => setState(!state)}
+            >
+              <Menu />
+            </button>
+          </div>
+        </div>
+        <div
+          className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+            state ? "block" : "hidden"
+          }`}
+        >
+          <ul className="justify-center items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+            {menus.map((item, idx) => (
+              <li key={idx} className="text-gray-600 hover:text-indigo-600">
+                <Link href={item.path}>{item.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
