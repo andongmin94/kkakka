4f46dcb7b672bf16696226651c3a3a3531f5de8e
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-01-16 17:06:55 +0900

    feat: Navbar making

--- frontend/components/nav-bar.tsx
+++ frontend/components/nav-bar.tsx
@@ -1,50 +1,114 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import Link from "next/link"
-import { Menu } from "lucide-react"
-import Image from "next/image"
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { usePathname, useSearchParams } from "next/navigation";
+import { BellIcon, UsersIcon } from "@heroicons/react/24/outline";
+import {
+  BellIcon as CheckedBellIcon,
+  UsersIcon as ChekedUsersIcon,
+} from "@heroicons/react/24/solid";
+import {DropdownMenuRadioGroupDemo} from "@/components/alarm-menu";
 
 export default function Navbar() {
-  const [state, setState] = React.useState(false)
+  const [state, setState] = useState(false);
 
-  const menus = [
-    { title: "Home", path: "/your-path" },
-    { title: "Blog", path: "/your-path" },
-    { title: "About Us", path: "/your-path" },
-    { title: "Contact Us", path: "/your-path" },
-  ]
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    // Add closing the navbar menu when navigating
+    const handleState = () => {
+      document.body.classList.remove("overflow-hidden");
+      setState(false);
+    };
+
+    handleState();
+  }, [pathname, searchParams]);
+
+  const handleNavMenu = () => {
+    setState(!state);
+    document.body.classList.toggle("overflow-hidden");
+  };
 
   return (
-    <nav className="bg-white w-full border-b md:border-0">
-      <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
-        <div className="flex items-center justify-between py-3 md:py-5 md:block">
-          <Link href="/">
-          <Image src="/logo.svg" alt="logo" width={150} height={130} priority />
-          </Link>
-          <div className="md:hidden">
-            <button
-              className="text-gray-700 outline-none p-2 rounded-md focus:border-gray-400 focus:border"
-              onClick={() => setState(!state)}
-            >
-              <Menu />
-            </button>
+    <header>
+      <nav
+        className={`bg-white shadow-lg w-full md:static md:text-sm ${
+          state ? "fixed z-10 h-full" : ""
+        }`}
+      >
+        <div className="custom-screen items-center mx-auto md:flex">
+          <div className="flex items-center justify-between py-4 md:py-5 md:block">
+            <Link href="/" className="flex items-center gap-3">
+              <Image
+                src="/logo.png"
+                alt="close"
+                className="mx-14"
+                width={100}
+                height={100}
+              />
+            </Link>
+            <div className="md:hidden">
+              <button
+                role="button"
+                aria-label="Open the menu"
+                className="text-gray-500 hover:text-gray-800"
+                onClick={handleNavMenu}
+              >
+                {state ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+          <div
+            className={`flex-1 pb-3 mt-8 md:pb-0 md:mt-0 md:block ${
+              state ? "" : "hidden"
+            }`}
+          >
+            <ul className="text-gray-700 justify-end items-center space-y-6 md:flex md:space-x-6 md:space-y-0 md:text-gray-600 md:font-medium">
+              <li>
+                <Link href="/">
+             <DropdownMenuRadioGroupDemo />
+                </Link>
+              </li>
+              <li>
+                <Link href="/">
+                  <UsersIcon className="w-10 h-10 mx-8 mr-16" />
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
-        <div
-          className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-            state ? "block" : "hidden"
-          }`}
-        >
-          <ul className="justify-center items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-            {menus.map((item, idx) => (
-              <li key={idx} className="text-gray-600 hover:text-indigo-600">
-                <Link href={item.path}>{item.title}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </nav>
-  )
+      </nav>
+    </header>
+  );
 }
