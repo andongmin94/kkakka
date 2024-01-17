d0948bb18d6258015dc8f1cd6f9f56236e803a24
lhgeer2617 <160981817+lhgeer7485@users.noreply.github.com>
2024-01-17 15:23:58 +0900

    Feat : S10P12D110-21-carousel 디자인 수정

--- frontend/components/ui/skeleton.tsx
+++ frontend/components/ui/skeleton.tsx
@@ -1,15 +1,10 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 function Skeleton({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
-      {...props}
-    />
-  )
+  return <div className={cn("", className)} {...props} />;
 }
 
-export { Skeleton }
+export { Skeleton };
