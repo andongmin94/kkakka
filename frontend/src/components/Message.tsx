50b13aada2218defbc59190501c1a92822d7463a
JIYEON KIM <jiyeon2536@naver.com>
2024-01-21 18:31:06 +0900

    Feat: S10P12D110-42 message list view
    메시지 리스트 뷰

---
+++ frontend/src/components/Message.tsx
@@ -0,0 +1,26 @@
+import MessagePeek from "./MessagePeek";
+import UnreadMessages from "./UnreadMessages";
+import UserName from "./UserName";
+import UserStatus from "./UserStatus";
+import UserCurrentAlias from "./UserCurrentAlias";
+import LiveProfile from "./LiveProfile";
+import classes from "./Message.module.css";
+import MessageUpdatedAt from "./MessageUpdatedAt";
+
+export default function Message({ messageInfo }) {
+  return (
+    <>
+      <div className={classes.wrapper}>
+        <LiveProfile text={"제목 떼주세유"} />
+        <div>
+          <UserCurrentAlias alias={messageInfo.alias} />
+          <UserName name={messageInfo.name} />
+          <UserStatus status={messageInfo.status} />
+        </div>
+        <MessagePeek lastMessage={messageInfo.lastMessage} />
+        <UnreadMessages />
+        <MessageUpdatedAt updatedAt={messageInfo.updatedAt} />
+      </div>
+    </>
+  );
+}
