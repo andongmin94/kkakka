781233b19930d109a3bf202bef01b340f9d56fd2
lhgeer2617 <lhgeer2617@gmail.com>
2024-01-22 17:51:35 +0900

    test2

--- frontend/src/components/main/LiveContent.tsx
+++ frontend/src/components/main/LiveContent.tsx
@@ -18,7 +18,9 @@ interface data {
 export default function LiveContent({ data }: { data: data }) {
   return (
     // 배너 이미지
-    <Card className="border-solid border-4 rounded-3xl bg-[url('image/liveImage.png')] bg-cover h-[15rem] w-[23rem] grid grid-rows-5 lg:hover:scale-105 transition-transform ease-in-out duration-500">
+    <Card
+      className={`border-solid border-4 rounded-3xl bg-[url('${data.bgImage}')] bg-cover h-[15rem] w-[23rem] grid grid-rows-5 lg:hover:scale-105 transition-transform ease-in-out duration-500`}
+    >
       <CardHeader className="p-1 grid grid-cols-2">
         <Live />
         <div className="flex justify-end">
@@ -27,7 +29,8 @@ export default function LiveContent({ data }: { data: data }) {
             return (
               <div
                 key={idx}
-                className={`rounded-full border-[3px] h-10 w-10 border-red-500 bg-[url('${friend.image}')] bg-cover`}
+                // 이유는 모르겠는데 여기서만 특정 이미지가 안나옴
+                className="rounded-full border-[3px] h-10 w-10 border-red-500 bg-[url('/image/joinSample.png')] bg-cover"
               />
             );
           })}
@@ -35,7 +38,7 @@ export default function LiveContent({ data }: { data: data }) {
       </CardHeader>
       <CardContent className="p-1 row-span-2"></CardContent>
       <CardFooter className="p-2 row-span-2">
-        {/* 플레이중인 친구 프사 (text에 내용을 적어주면 props로 전달) */}
+        {/* 플레이중인 친구 프사 props로 전달) */}
         <LiveProfile
           userId={data.userId}
           text={data.title}
