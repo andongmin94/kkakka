a6e709652db2ede4c7ab3446696d5a0f61616e97
lhgeer2617 <lhgeer2617@gmail.com>
2024-01-22 16:43:02 +0900

    test
    Details:
    - Commit: d327ee742b393fc15a4645e789ca392016e01a9a
    - Author: lhgeer2617 <lhgeer2617@gmail.com>
    - Date: 2024-01-22
    - Parent count: 1
    - Line changes: +1273 / -26
    - Files:
    - M: frontend/index.html
    - M: frontend/src/components/TitleItemShop.tsx
    - M: frontend/src/components/ani/Live.tsx
    - A: frontend/src/components/main/LiveContent.tsx
    - A: frontend/src/components/main/LiveContentCarousel.tsx
    - A: frontend/src/components/main/LiveProfile.tsx
    - A: frontend/src/components/message/Message.module.css
    - A: frontend/src/components/message/Message.tsx
    - A: frontend/src/components/message/MessagePeek.tsx
    - A: frontend/src/components/message/MessageProfile.tsx
    - A: frontend/src/components/message/MessageUpdatedAt.tsx
    - A: frontend/src/components/navbar/Alarm.module.css
    - A: frontend/src/components/navbar/Alarm.tsx
    - A: frontend/src/components/navbar/FriendsBtn.module.css
    - A: frontend/src/components/navbar/FriendsBtn.tsx
    - A: frontend/src/components/navbar/ModeToggle.tsx
    - A: frontend/src/components/navbar/ThemeProvider.tsx
    - A: frontend/src/components/navbar/friendsSidebar/FriendsCard.module.css
    - A: frontend/src/components/navbar/friendsSidebar/FriendsCard.tsx
    - A: frontend/src/components/navbar/friendsSidebar/FriendsProfile.tsx
    - A: frontend/src/components/navbar/friendsSidebar/UserCurrentAlias.module.css
    - A: frontend/src/components/navbar/friendsSidebar/UserCurrentAlias.tsx
    - A: frontend/src/components/navbar/friendsSidebar/UserName.module.css
    - A: frontend/src/components/navbar/friendsSidebar/UserName.tsx
    - A: frontend/src/components/navbar/friendsSidebar/UserStatus.module.css
    - A: frontend/src/components/navbar/friendsSidebar/UserStatus.tsx
    - A: frontend/src/live.json
    - M: frontend/src/main.tsx
    - M: frontend/src/routes/IndexPage.tsx
    - M: frontend/src/routes/MessagePage.tsx
    - M: frontend/src/routes/RootLayout.module.css
    - M: frontend/src/routes/RootLayout.tsx

---
+++ frontend/src/components/navbar/FriendsBtn.tsx
@@ -0,0 +1,56 @@
+import FriendsCard from "./friendsSidebar/FriendsCard";
+import classes from "@/components/navbar/FriendsBtn.module.css";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+// 친구 카드 여러개를 띄우기 위한 더미 데이터
+
+const friendsInfo = [
+  {
+    name: "김상훈",
+    alias: "인의동손잭스",
+    status: "플레이 중",
+    image: "/image/profileImage.png",
+  },
+  {
+    name: "이수민",
+    alias: "냥냥펀치냥냥",
+    status: "플레이 중",
+    image: "/image/profileImage.png",
+  },
+  {
+    name: "이해건",
+    alias: "이해건삼해건",
+    status: "플레이 중",
+    image: "/image/profileImage.png",
+  },
+];
+
+export default function FriendsBtn() {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <div className={classes.friends_image} />
+      </SheetTrigger>
+
+      <SheetContent className="p-0">
+        <SheetHeader>
+          <SheetTitle className="grid place-items-center text-2xl border-2 rounded-lg shadow-xl mt-[6px] mx-1 bg-[#f14e4e91]">
+            <p>친구목록</p>
+          </SheetTitle>
+          <div className={classes.scrollbar}>
+            {/* 친구 카드 생성 */}
+            {friendsInfo.map((info, idx) => {
+              return <FriendsCard key={idx} info={info} />;
+            })}
+          </div>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+}
