cc8ab58e6e707725221506676b636fc573399cd8
오세영 <osy9536@naver.com>
2024-01-22 15:31:50 +0900

    Feat : 실행시간 측정 aop 개발
    S10P12D110-171 #done #comment 닫기

--- backend/src/main/java/org/ssafy/ssafy_common2/SsafyCommon2Application.java
+++ backend/src/main/java/org/ssafy/ssafy_common2/SsafyCommon2Application.java
@@ -3,8 +3,10 @@ package org.ssafy.ssafy_common2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SsafyCommon2Application {
 
 	public static void main(String[] args) {
