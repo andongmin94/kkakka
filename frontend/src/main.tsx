c751baabdff5ee8b452da72bcd2fdc07498150b0
JIYEON KIM <jiyeon2536@naver.com>
2024-01-23 13:30:45 +0900

    Feat/로그인 : 카카오로그인 프론트 구현
    카카오 OAuth 로그인 프론트엔드단 구현함.
    
    S10P12D110-103 #done
    
    Details:
    - Commit: 88951fc9dbe1fc46c5cf5b8a9056247a343f0909
    - Author: JIYEON KIM <jiyeon2536@naver.com>
    - Date: 2024-01-23
    - Parent count: 1
    - Line changes: +49 / -0
    - Files:
    - M: frontend/.gitignore
    - A: frontend/src/components/Login.tsx
    - M: frontend/src/main.tsx
    - A: frontend/src/routes/KakaoCallbackPage.tsx
    - A: frontend/src/routes/LoginPage.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,9 +4,11 @@ import ReactDOM from "react-dom/client";
 import IndexPage from "@/routes/IndexPage";
 import IntroPage from "@/routes/IntroPage.tsx";
 import RootLayout from "@/routes/RootLayout.tsx";
+import LoginPage from "@/routes/LoginPage.tsx";
 import MessagePage from "@/routes/MessagePage.tsx";
 import SettingPage from "@/routes/SettingPage.tsx";
 import ItemshopPage from "@/routes/ItemshopPage.tsx";
+import KakaoCallbackPage from "@/routes/KakaoCallbackPage.tsx";
 import { ThemeProvider } from "@/components/navbar/ThemeProvider";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -22,6 +24,11 @@ const router = createBrowserRouter([
       { path: "/message", element: <MessagePage /> },
       { path: "/setting", element: <SettingPage /> },
       { path: "/intro", element: <IntroPage /> },
+      { path: "/login", element: <LoginPage /> },
+      {
+        path: "/api/oauth/callback/kakao/token",
+        element: <KakaoCallbackPage />,
+      },
     ],
   },
 ]);
