d0b4a93613e7da24016c2b6ac7473f64faf05ad2
오세영 <osy9536@naver.com>
2024-01-23 17:07:43 +0900

    Feat/도감 : 도감 추가하기
    S3연동 완료, 도감과 아이템 거래내역 간 양방향 매핑, 두 엔티티간 연관관계 편의 매서드 적용, 포인트 차감 로직 수정해야함, 응답으로 요청으로 받은 이미지를 url로 변환시킨 값과 도감 제목을 반환함
    
    S10P12D110-163 #done #comment 닫기
    
    Details:
    - Commit: 88d2d93e42ee51d6f48ce4d38d7fbc67988acb5d
    - Author: 오세영 <osy9536@naver.com>
    - Date: 2024-01-23
    - Parent count: 1
    - Line changes: +520 / -86
    - Files:
    - M: backend/build.gradle
    - A: backend/src/main/java/org/ssafy/ssafy_common2/_common/config/AwsS3Config.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/_common/config/WebSecurityConfig.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/_common/exception/ErrorType.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/_common/response/MsgType.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/_common/service/S3Uploader.java
    - D: backend/src/main/java/org/ssafy/ssafy_common2/collection/entity/CommentDogam.java
    - D: backend/src/main/java/org/ssafy/ssafy_common2/collection/entity/DislikeDogam.java
    - D: backend/src/main/java/org/ssafy/ssafy_common2/collection/entity/Dogam.java
    - D: backend/src/main/java/org/ssafy/ssafy_common2/collection/repository/CommentDogamRepository.java
    - D: backend/src/main/java/org/ssafy/ssafy_common2/collection/repository/DislikeDogamRepository.java
    - D: backend/src/main/java/org/ssafy/ssafy_common2/collection/repository/DogamRepository.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/dogam/controller/DogamController.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/dogam/dto/reqeust/DogamCreateRequestDto.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/dogam/dto/response/DogamCreateResponseDto.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/dogam/entity/CommentDogam.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/dogam/entity/DislikeDogam.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/dogam/entity/Dogam.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/dogam/repository/CommentDogamRepository.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/dogam/repository/DislikeDogamRepository.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/dogam/repository/DogamRepository.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/dogam/service/DogamService.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/itemshop/entity/Enforcement.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/itemshop/entity/ItemDealList.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/itemshop/entity/ItemShop.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/itemshop/repository/EnforcementRepository.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/itemshop/repository/ItemShopRepository.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/itemshop/repository/ItempDealListRepository.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/user/entity/Alias.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/user/entity/DynamicUserInfo.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/user/entity/User.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/user/service/UserService.java
    - M: backend/src/main/resources/application-dev.yml
    - M: backend/src/main/resources/application.yml

--- backend/src/main/java/org/ssafy/ssafy_common2/dogam/repository/DogamRepository.java
+++ backend/src/main/java/org/ssafy/ssafy_common2/dogam/repository/DogamRepository.java
@@ -1,7 +1,7 @@
-package org.ssafy.ssafy_common2.collection.repository;
+package org.ssafy.ssafy_common2.dogam.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.ssafy.ssafy_common2.collection.entity.Dogam;
+import org.ssafy.ssafy_common2.dogam.entity.Dogam;
 
 public interface DogamRepository extends JpaRepository<Dogam,Long> {
 }
--- backend/src/main/java/org/ssafy/ssafy_common2/itemshop/entity/Enforcement.java
+++ backend/src/main/java/org/ssafy/ssafy_common2/itemshop/entity/Enforcement.java
@@ -1,6 +1,6 @@
 package org.ssafy.ssafy_common2.itemshop.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +10,10 @@ import org.ssafy.ssafy_common2._common.entity.BaseTime;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Enforcement extends ItemDealList {
+public class Enforcement extends BaseTime {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 }
