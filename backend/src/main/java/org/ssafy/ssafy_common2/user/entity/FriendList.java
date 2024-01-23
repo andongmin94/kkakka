7d436e3ff4730c2a0e6f226e136b17fd6f3fd36b
oistmil <oistmil@gmail.com>
2024-01-23 17:29:40 +0900

    Feat/친구 : 친구 Entity 설계 및 친구 추가 기능 구현
    - FriendList 데이터 관련 클래스 생성
    - 친구 추가 요청에 대한 Controller 메서드 생성
    - 친구 추가에 필요한 Service 메서드 생성
    
    - postman 테스트 결과 이상 없습니다.
    
    현재 두 사람의 친구 상태를 확인하는 getFriendState() 메서드 내부 구현과 외부 사용이 비효율적으로 느껴지는데, 머리가 안돌아가서 담에 리팩토링 해보겠습니다. (좋은 방법 있다면 알려주시와요)
    
    S10P12D110-148 #done #comment 닫기
    S10P12D110-150 #done #comment 닫기

--- backend/src/main/java/org/ssafy/ssafy_common2/user/entity/FriendList.java
+++ backend/src/main/java/org/ssafy/ssafy_common2/user/entity/FriendList.java
@@ -2,18 +2,52 @@ package org.ssafy.ssafy_common2.user.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.ssafy.ssafy_common2._common.entity.BaseTime;
 
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class FriendList {
+public class FriendList extends BaseTime {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender", nullable = false)
+    private User sender;
+
+    @Column(name = "receiver", nullable = false, length = 50)
+    private String receiver;
+
+    @Column(name = "is_check", nullable = false)
+    private Boolean isCheck = false;
+
+    @Builder
+    private FriendList(User sender, String receiver, Boolean isCheck){
+
+        this.sender = sender;
+        this.receiver = receiver;
+        this.isCheck = isCheck;
+    }
+
+    public static FriendList of(User sender, String receiver, Boolean isCheck){
+
+        return builder()
+                .sender(sender)
+                .receiver(receiver)
+                .isCheck(isCheck)
+                .build();
+    }
+
+    public void updateIsCheck(Boolean isCheck){
+        this.isCheck = isCheck;
+    }
 
 }
