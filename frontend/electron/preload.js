64ee2dd324a663d87e5d56302886d8cb344a47e4
김상훈 <k1016h@naver.com>
2024-01-23 19:57:59 +0900

    Feat/일렉트론 빌드 세팅 완료 -> 후행으로 리액트 빌드 폴리싱 해야함
    Details:
    - Commit: 0d3a7699edcc9132172c31494b71d384257fecbc
    - Author: 김상훈 <k1016h@naver.com>
    - Date: 2024-01-23
    - Parent count: 1
    - Line changes: +1765 / -62
    - Files:
    - M: frontend/electron/main.cjs
    - M: frontend/electron/preload.js
    - M: frontend/index.html
    - M: frontend/package-lock.json
    - M: frontend/package.json
    - D: frontend/public/D110.psd
    - M: frontend/src/components/ani/Live.tsx
    - M: frontend/src/main.tsx
    - M: frontend/vite.config.ts

--- frontend/electron/preload.js
+++ frontend/electron/preload.js
@@ -1,7 +1,11 @@
-const { contextBridge, ipcRenderer } = require('electron')
-const { currentLoad } = require('systeminformation')
-
-contextBridge.exposeInMainWorld("api", {
-    close: () => ipcRenderer.send('close-app'),
-    getCurrentLoad: () => currentLoad()
-})
+window.addEventListener("DOMContentLoaded", () => {
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector);
+      if (element) element.innerText = text;
+    };
+  
+    for (const dependency of ["chrome", "node", "electron"]) {
+      replaceText(`${dependency}-version`, process.versions[dependency]);
+    }
+  });
+  
