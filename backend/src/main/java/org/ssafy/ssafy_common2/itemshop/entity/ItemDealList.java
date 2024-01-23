c29208a6a5928b5c9fac8d2839faf73b1f32e958
oistmil <75559067+oistmil@users.noreply.github.com>
2024-01-23 12:38:00 +0900

    Feat/아이템샵 : 아이템샵 데이터 클래스 작성

--- backend/src/main/java/org/ssafy/ssafy_common2/itemshop/entity/ItemDealList.java
+++ backend/src/main/java/org/ssafy/ssafy_common2/itemshop/entity/ItemDealList.java
@@ -2,20 +2,35 @@ package org.ssafy.ssafy_common2.itemshop.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.ssafy.ssafy_common2._common.entity.BaseTime;
+import org.ssafy.ssafy_common2.user.entity.User;
 
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn
-public class ItemDealList {
+public class ItemDealList extends BaseTime {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_type", nullable = false)
+    private ItemShop itemType;
+
+    @Builder
+    public ItemDealList(Long id, User user, ItemShop itemType) {
+        this.id = id;
+        this.user = user;
+        this.itemType = itemType;
+    }
 }
--- backend/src/main/java/org/ssafy/ssafy_common2/itemshop/entity/ItemShop.java
+++ backend/src/main/java/org/ssafy/ssafy_common2/itemshop/entity/ItemShop.java
@@ -2,18 +2,37 @@ package org.ssafy.ssafy_common2.itemshop.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.ssafy.ssafy_common2._common.entity.BaseTime;
 
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ItemShop {
+public class ItemShop extends BaseTime {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @Column(name = "item_name", nullable = false, length = 20)
+    String itemName;
+
+    @Column(name = "item_price", nullable = false, length = 30)
+    String itemPrice;
+
+    @Column(name = "item_desc", nullable = false, length = 255)
+    String itemDesc;
+
+    @Builder
+    public ItemShop(Long id, String itemName, String itemPrice, String itemDesc) {
+        this.id = id;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.itemDesc = itemDesc;
+    }
 
 }
