c1f57b8c88a07493552ed1347414e52d286c1645
Jeon Soo Min <wjsaos2081@gmail.com>
2024-01-24 20:27:25 +0900

    Refactor/채팅 : build.gradle, yml 파일 설정 및 수민,세영이형 피드백 반영 리팩토링
    build.gradle, yml 파일 설정 추가
    massage -> message 로 바꾸기
    chatRoom Builder에서 auto_increment 내용인 id 삭제
    
    Change-Id: Ib91902a327087a5babb4010c6857124027abfd4b
    
    Details:
    - Commit: d5d18e6baf01f6bf1125b6d64877284061861c7b
    - Author: Jeon Soo Min <wjsaos2081@gmail.com>
    - Date: 2024-01-24
    - Parent count: 1
    - Line changes: +14 / -10
    - Files:
    - M: backend/build.gradle
    - D: backend/src/main/java/org/ssafy/ssafy_common2/chatting/entity/Massage.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/chatting/entity/Message.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/chatting/repository/ChatJoinRepository.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/chatting/repository/ChatRoomRepository.java
    - D: backend/src/main/java/org/ssafy/ssafy_common2/chatting/repository/MassageRepository.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/chatting/repository/MessageRepository.java
    - M: backend/src/main/resources/application.yml

--- backend/src/main/java/org/ssafy/ssafy_common2/chatting/repository/MessageRepository.java
+++ backend/src/main/java/org/ssafy/ssafy_common2/chatting/repository/MessageRepository.java
@@ -3,5 +3,6 @@ package org.ssafy.ssafy_common2.chatting.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.ssafy.ssafy_common2.chatting.entity.Message;
 
-public interface ChatJoinRepository extends JpaRepository<Message,Long> {
+public interface MessageRepository extends JpaRepository<Message,Long> {
+
 }
