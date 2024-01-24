48c775aca683e7598ba840d31f359b1621a043c1
lhgeer2617 <160981817+lhgeer7485@users.noreply.github.com>
2024-01-24 17:24:52 +0900

    Feat/아이템샵: 도감삭제권 제외 구현 완료

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9bc6ijbadco"
+    "revision": "0.rjo15fvee78"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- frontend/src/components/itemShop/DeleteCollection.tsx
+++ frontend/src/components/itemShop/DeleteCollection.tsx
@@ -1,11 +1,24 @@
-import { Link } from "react-router-dom";
 import { Card } from "@/components/ui/card";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogClose,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import Price from "./Price";
+import Purchase from "./Purchase";
 
 export default function TitleItemshop() {
   return (
     <Card className="static group/item bg-[url('/image/deleteAliasBg.png')] border-solid border-4 rounded-3xl bg-cover h-[23rem] w-[23rem] lg:hover:scale-105 transition-transform ease-in-out duration-500">
       <div className="flex flex-col items-center">
-        <img src="/image/deleteCollection.png" className="h-20 w-20 pt-3" />
+        <img src="/image/deleteCollection.png" className="h-20 w-20" />
         <p className="text-4xl mt-3 font-bold text-white">도감 삭제권</p>
         <p className="text-xl mt-10 font-bold text-white mx-10">
           내 도감에 들어있는 게시물을 1개 선택하여 삭제하는 아이템이다. 본인의
@@ -16,27 +29,59 @@ export default function TitleItemshop() {
 
       {/* 호버 */}
       <div className="opacity-50 absolute top-[-4px] right-[-4px] border-solid border-4 rounded-3xl bg-slate-400 h-[23rem] w-[23rem] group/edit invisible group-hover/item:visible" />
-      <div className="group/edit invisible group-hover/item:visible h-[23rem] w-[23rem] grid place-items-center z-10">
-        <Link
-          to="/"
-          className="absolute top-[150px] rounded-xl h-[4rem] w-[15rem] grid place-items-center bg-white z-20 font-dnf lg:hover:scale-105 transition-transform ease-in-out duration-500"
-        >
-          <div className="flex flex-row justify-content-center gap-4">
-            <img src="/image/coins.png" className="h-10 w-10" />
-            <span className="self-center text-2xl font-bold">1,000 P</span>
+      <Dialog>
+        <DialogTrigger asChild>
+          <div className="group/edit invisible group-hover/item:visible h-[23rem] w-[23rem] grid place-items-center z-10">
+            {/* 호버 가격 버튼 */}
+            <Price />
+
+            {/* 호버 구매하기 버튼 */}
+            <Purchase />
           </div>
-        </Link>
-        <Link
-          to="/"
-          className="absolute top-[250px] rounded-xl h-[4rem] w-[15rem] grid place-items-center bg-slate-500 z-20 font-dnf lg:hover:scale-105 transition-transform ease-in-out duration-500"
-        >
-          <div className="flex flex-row justify-content-center gap-4">
-            <span className="self-center text-2xl text-white font-bold">
-              구매하기
-            </span>
+        </DialogTrigger>
+
+        {/* 모달 부분 */}
+        <DialogContent className="sm:max-w-[480px]">
+          <DialogHeader>
+            <DialogTitle className="flex flex-col items-center text-3xl">
+              <div className="mb-3">도감 삭제권</div>
+              <div className="rounded-xl h-[4rem] w-[15rem] grid place-items-center bg-white">
+                <div className="flex flex-row justify-content-center gap-4">
+                  <img src="/image/coins.png" className="h-10 w-10" />
+                  <span className="self-center text-2xl font-bold">
+                    1,000 P
+                  </span>
+                </div>
+              </div>
+            </DialogTitle>
+          </DialogHeader>
+
+          {/* 본문 */}
+          <div className="flex justify-center w-full mb-5 mt-5" />
+          <div className="font-bold text-center mb-3">
+            <div>도감을 어떻게 선택할지 아직 몰루</div>
+            구입 후 잔여 포인트 4000 P
           </div>
-        </Link>
-      </div>
+          <DialogFooter className="flex sm:justify-center">
+            <DialogClose asChild>
+              <Button
+                type="button"
+                variant="secondary"
+                className="mr-10 border-solid border-2 border-inherit bg-white font-bold h-8 text-lg"
+              >
+                취소
+              </Button>
+            </DialogClose>
+            <Button
+              type="submit"
+              variant="secondary"
+              className="mr-10 border-solid border-2 border-inherit bg-white font-bold h-8 text-lg"
+            >
+              구입
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </Card>
   );
 }
