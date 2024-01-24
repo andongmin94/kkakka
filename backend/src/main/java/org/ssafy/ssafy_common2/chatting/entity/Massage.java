8fe501f5620ed5b24f789df927de693db5aa4daf
Jeon Soo Min <wjsaos2081@gmail.com>
2024-01-24 17:21:37 +0900

    feat/채팅: Entity 연관관계 작성 완료 및, 소켓 통신을 위한 WebSecurityConfig 추가 및 Redis 연동
    - 채팅 부분 Entity ERD에 맞게 수정 하였습니다.
    - 해당 부분 진행하며 ERD에 chat-room이 모두 char-room으로 되어있어서 ERD도 수정하였습니다.
    - Redis 연동을 위한 의존성 추가 및, Config Class 생성 하였습니다.
    
    - postman 테스트 결과 이상 없습니다
    
    Change-Id: I2db215d590bdd0ac6e65ff81e8cbb1ea253dba7a
    
    Details:
    - Commit: b3faab00c28776802f4eb71101003c32082e8b30
    - Author: Jeon Soo Min <wjsaos2081@gmail.com>
    - Date: 2024-01-24
    - Parent count: 1
    - Line changes: +337 / -10
    - Files:
    - M: backend/src/main/java/org/ssafy/ssafy_common2/_common/config/WebSecurityConfig.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/chatting/config/RedisConfig.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/chatting/config/WebSocketConfig.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/chatting/config/handler/StompHandler.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/chatting/entity/ChatJoin.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/chatting/entity/ChatJoinId.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/chatting/entity/ChatRoom.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/chatting/entity/CompositeKeyForMsg.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/chatting/entity/Massage.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/chatting/service/ChatRoomRedisService.java

--- backend/src/main/java/org/ssafy/ssafy_common2/chatting/entity/Massage.java
+++ backend/src/main/java/org/ssafy/ssafy_common2/chatting/entity/Massage.java
@@ -2,17 +2,32 @@ package org.ssafy.ssafy_common2.chatting.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.ssafy.ssafy_common2._common.entity.BaseTime;
 
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Massage {
+
+public class Massage extends BaseTime  {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @Column(name = "content")
+    private String name;
+
+    @ManyToOne
+    @MapsId("userId")
+    @JoinColumns({
+            @JoinColumn(name = "user_id"),
+            @JoinColumn(name = "chat_room_id")
+    })
+    private  ChatJoin chatJoin;
+
 }
