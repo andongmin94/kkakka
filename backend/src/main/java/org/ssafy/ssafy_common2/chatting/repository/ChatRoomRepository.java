42a6ad3e60b9ad638128bd54fb68a8c589105b42
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-01-25 14:28:57 +0900

    Refactor/채팅 : SQL DELETE RECORD 시 자동 delete_at 입력 되도록 Repository Entity 수정

--- backend/src/main/java/org/ssafy/ssafy_common2/chatting/repository/ChatRoomRepository.java
+++ backend/src/main/java/org/ssafy/ssafy_common2/chatting/repository/ChatRoomRepository.java
@@ -1,7 +1,11 @@
 package org.ssafy.ssafy_common2.chatting.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.ssafy.ssafy_common2.chatting.entity.Message;
+import org.ssafy.ssafy_common2.chatting.entity.ChatRoom;
 
-public interface ChatRoomRepository extends JpaRepository<Message,Long> {
+import java.util.List;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom,Long> {
+
+    List<ChatRoom> findAllByChatOwnerEmailAndDeletedAtIsNull(String chat_owner_email);
 }
