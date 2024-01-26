79cfe839877ee3609a515e06e08edbdef521d14b
김상훈 <k1016h@naver.com>
2024-01-26 17:30:02 +0900

    Chore:sync

--- frontend/src/routes/SettingPage.tsx
+++ frontend/src/routes/SettingPage.tsx
@@ -1,3 +1,34 @@
-export default function SettingPage() {
-  return <>SETTING</>;
+declare global {
+  interface Window {
+    electron: typeof import('electron');
+  }
 }
+
+const electron =  window.electron;
+import { Slider } from "@/components/ui/slider"
+
+export default function SettingPage () {
+  const handleClick = () => {
+    electron.ipcRenderer.send('button-clicked', 'hi');
+    console.log('React button clicked');
+  };
+  const closeClick = () => {
+    electron.ipcRenderer.send('close-clicked');
+    console.log('React close clicked');
+  }
+
+  return (
+    <div>
+      <div className="w-80">
+      <Slider />
+      </div>
+      <button onClick={handleClick}>
+        Click Me
+      </button>
+
+      <button onClick={closeClick}>
+        Click Me
+      </button>
+    </div>
+  );
+};
