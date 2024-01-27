6e29524be56bebfaf2847997cd6f3ae04b17edc2
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-01-27 19:43:34 +0900

    fix/커밋 에러 수정

--- frontend/src/components/main/LiveProfile.tsx
+++ frontend/src/components/main/LiveProfile.tsx
@@ -1,9 +1,9 @@
 import { cn } from "@/lib/utils";
 
 interface LiveProfileProps {
-  userId: number;
-  text: string;
-  profileImage: string;
+  playerName: string;
+  roomTitle: string;
+  playerProfilePic: string;
 }
 
 function Skeleton({
@@ -13,17 +13,21 @@ function Skeleton({
   return <div className={cn("rounded-md", className)} {...props} />;
 }
 
-export default function LiveProfile({ text, profileImage }: LiveProfileProps) {
+export default function LiveProfile({
+  playerName,
+  roomTitle,
+  playerProfilePic,
+}: LiveProfileProps) {
   return (
     <div className="flex items-center space-x-3">
       <Skeleton
-        className={`w-[80px] h-[80px] rounded-full border-4  border-red-500 bg-[url('${profileImage}')] bg-cover`}
+        className={`w-[80px] h-[80px] rounded-full border-4  border-red-500 bg-[url('${playerProfilePic}')] bg-cover`}
       />
       <div className="space-y-2">
         <Skeleton className="h-auto grid grid-rows-1 place-items-stretch w-[250px]">
           {/* 제목 칸 */}
           <div className="self-center text-2xl px-1 overflow-hidden font-bold">
-            {text}
+            {roomTitle}
           </div>
         </Skeleton>
       </div>
