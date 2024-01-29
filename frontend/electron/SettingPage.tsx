62231bddbb021badae25da2f3cbfdbff57df71e8
김상훈 <k1016h@naver.com>
2024-01-29 17:59:55 +0900

    Feat : Riot sync
    Details:
    - Commit: a87462186bc059d6aa0d59bf2e621efd31115d13
    - Author: 김상훈 <k1016h@naver.com>
    - Date: 2024-01-29
    - Parent count: 1
    - Line changes: +185 / -49
    - Files:
    - M: frontend/.env
    - M: frontend/electron/SettingPage.tsx
    - M: frontend/electron/TitleBar.tsx
    - A: frontend/electron/index.js
    - M: frontend/electron/main.cjs
    - M: frontend/package-lock.json
    - M: frontend/package.json
    - M: frontend/src/main.tsx
    - M: frontend/src/routes/RootLayout.module.css

--- frontend/electron/SettingPage.tsx
+++ frontend/electron/SettingPage.tsx
@@ -1,28 +1,44 @@
+import { Slider } from "@/components/ui/slider";
+import axios from "axios";
+import https from "https";
+
 declare global {
   interface Window {
-    electron: typeof import('electron');
+    electron: typeof import("electron");
   }
 }
 
-const electron =  window.electron;
-import { Slider } from "@/components/ui/slider"
+const electron = window.electron;
 
-export default function SettingPage () {
+export default function SettingPage() {
   const handleClick = () => {
-    electron.ipcRenderer.send('button-clicked', 'hi');
-    console.log('React button clicked');
+    electron.ipcRenderer.send("button-clicked", "hi");
+    console.log("React button clicked");
   };
+  const gameInfo = async () => {
+    const response = await axios({
+      method: "get",
+      url: "https://127.0.0.1:2999/liveclientdata/eventdata",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false, // 허가되지 않은 인증을 reject하지 않겠다!
+      }),
+    });
 
+    electron.ipcRenderer.send("Riot Game Info", response.data);
+  };
 
   return (
     <div>
       <div className="w-80">
-      <Slider />
+        <Slider />
       </div>
-      <button onClick={handleClick}>
-        Click Me
-      </button>
+      <button onClick={handleClick}>Click Me</button>
 
+      <button onClick={gameInfo}>Click Me</button>
     </div>
   );
-};
+}
