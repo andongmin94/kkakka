abd245682d5a5a82c9bcc555031e933b46f117eb
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-01-29 17:48:19 +0900

    Fix: S10P12D110-181 live content carousel 에러 고침

--- frontend/src/types/storeTypes.ts
+++ frontend/src/types/storeTypes.ts
@@ -16,7 +16,7 @@ export interface friendStoreType {
 }
 
 export interface alarmStoreType {
-  alarms: [];
+  alarms: [] | null;
   fetchAlarms: () => void;
   checkAlarm: (alarmId: number) => void;
 }
