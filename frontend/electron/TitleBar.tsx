374fce7d2067ccd8358e9a90346c1d166b6d0003
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-01-29 17:59:55 +0900

    feat: Riot sync

--- frontend/electron/TitleBar.tsx
+++ frontend/electron/TitleBar.tsx
@@ -1,30 +1,33 @@
 declare global {
   interface Window {
-    electron: typeof import('electron');
+    electron: typeof import("electron");
   }
 }
 
-const electron =  window.electron;
+const electron = window.electron;
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Menubar } from "@/components/ui/menubar";
 
 export default function TitleBar() {
   const minimize = () => {
-    electron.ipcRenderer.send('minimize');
+    electron.ipcRenderer.send("minimize");
   };
   const maximize = () => {
-    electron.ipcRenderer.send('maximize');
-  }
+    electron.ipcRenderer.send("maximize");
+  };
   const hidden = () => {
-    electron.ipcRenderer.send('hidden');
-  }
+    electron.ipcRenderer.send("hidden");
+  };
   return (
-    <div className="flex justify-end border-blue-200 mt-2 mb-10">
+
+    <Menubar className="fixed flex justify-end mt-2 bg-background sticky top-0 z-40 w-full border-b" style={{ '-webkit-app-region': 'drag' } as React.CSSProperties}>
       <div className="fixed z-10">
-      <Button onClick={minimize}>-</Button>&nbsp;
-      <Button onClick={maximize}>ㅁ</Button>&nbsp;
-      <Button onClick={hidden}>X</Button>
+        <Button onClick={minimize} style={{ '-webkit-app-region': 'no-drag' } as React.CSSProperties}>-</Button>&nbsp;
+        <Button onClick={maximize} style={{ '-webkit-app-region': 'no-drag' } as React.CSSProperties}>ㅁ</Button>&nbsp;
+        <Button onClick={hidden} style={{ '-webkit-app-region': 'no-drag' } as React.CSSProperties}>X</Button>
       </div>
-    </div>
-  )
+    </Menubar>
+
+  );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -18,14 +18,10 @@ import { RouterProvider, createHashRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 /////////////////////////////////////////////////////
 // 일렉트론 빌드 때만 사용되는 컴포넌트
-// import TitleBar from "../electron/TitleBar.tsx";
-// import SettingPage from "../electron/SettingPage.tsx";
+import TitleBar from "../electron/TitleBar.tsx";
+import SettingPage from "../electron/SettingPage.tsx";
 /////////////////////////////////////////////////////
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import ProfileCollection from "./routes/ProfileCollection";
-import ProfileDishonorPage from "./routes/ProfileDishonorPage";
-import ProfileRecordPage from "./routes/ProfileRecordPage";
-
 const queryClient = new QueryClient();
 const router = createHashRouter([
   {
@@ -40,7 +36,7 @@ const router = createHashRouter([
         element: <MessageDetailPage user={undefined} />,
       },
 
-      // { path: "/setting", element: <SettingPage /> },
+      { path: "/setting", element: <SettingPage /> },
       { path: "/intro", element: <IntroPage /> },
       { path: "/login", element: <LoginPage /> },
       {
