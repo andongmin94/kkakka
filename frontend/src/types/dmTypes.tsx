dd482c62c024947d5e3c2809ab337e7e1f7fae0e
JIYEON KIM <jiyeon2536@naver.com>
2024-01-29 12:55:49 +0900

    Feat : S10P12D110-184 타입정의
    - typescript 타입 정의
    S10P12D110-184 #done
    
    Details:
    - Commit: 7a0270e73ab335b409257b2880a6a9b0dad12d1d
    - Author: JIYEON KIM <jiyeon2536@naver.com>
    - Date: 2024-01-29
    - Parent count: 1
    - Line changes: +67 / -0
    - Files:
    - A: frontend/src/types/alarmTypes.tsx
    - A: frontend/src/types/aliasTypes.tsx
    - A: frontend/src/types/broadcastTypes.tsx
    - A: frontend/src/types/dmTypes.tsx
    - A: frontend/src/types/dogamTypes.tsx
    - A: frontend/src/types/friendTypes.tsx

---
+++ frontend/src/types/dmTypes.tsx
@@ -0,0 +1,11 @@
+export interface DmType {
+  dmId: number;
+  friendEmail: string;
+  friendProfilePic: string;
+  friendName: string;
+  friendAlias: string;
+  friendStatus: string;
+  lastMessage: string;
+  unreadMessage: number;
+  modifiedAt: Date;
+}
