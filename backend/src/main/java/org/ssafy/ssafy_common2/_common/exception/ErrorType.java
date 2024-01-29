10eb83b63cf574fc970b46529b124c54c199db0c
오세영 <osy9536@naver.com>
2024-01-29 19:16:05 +0900

    Feat : S10P12D110-160 도감 메인페이지 리스트
    도감 메인페이지에서 리스트로 불러오는 API 작성
    
    S10P12D110-160 #done #comment 닫기
    
    Details:
    - Commit: 406894eb03793316fead6905432d2dce2c26a460
    - Author: 오세영 <osy9536@naver.com>
    - Date: 2024-01-29
    - Parent count: 1
    - Line changes: +260 / -159
    - Files:
    - M: backend/src/main/java/org/ssafy/ssafy_common2/_common/exception/ErrorType.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/_common/response/MsgType.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/dogam/controller/DogamController.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/dogam/entity/CommentDogam.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/dogam/entity/DislikeDogam.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/dogam/entity/Dogam.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/dogam/repository/CommentDogamRepository.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/dogam/repository/DislikeDogamRepository.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/dogam/service/DogamService.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/user/controller/FriendListController.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/user/repository/AliasRepository.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/user/service/FriendListService.java

--- backend/src/main/java/org/ssafy/ssafy_common2/_common/exception/ErrorType.java
+++ backend/src/main/java/org/ssafy/ssafy_common2/_common/exception/ErrorType.java
@@ -25,8 +25,9 @@ public enum ErrorType {
     NOT_FOUND_DOGAM(401, "도감이 존재하지 않습니다."),
     NOT_MATCHING_DOGAM_USER(401, "유저와 도감이 매치되지 않습니다."),
     DUPLICATED_REQUEST(409, "이미 완료된 요청입니다."),
-    FAILED_TO_MAKE_CHATROOM(500, "채팅방 생성에 실패하였습니다.")
-    ;
+    FAILED_TO_MAKE_CHATROOM(500, "채팅방 생성에 실패하였습니다."),
+    NOT_FOUND_ALIAS(401, "칭호가 존재하지 않습니다."),
+    NOT_FOUND_COMMENT(401, "댓글을 작성해 주세요");
 
     private int code;
     private String msg;
