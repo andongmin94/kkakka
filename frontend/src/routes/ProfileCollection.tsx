04692c03e20c8e8dcdc286bdfa6f80e957bd1206
lhgeer2617 <lhgeer2617@gmail.com>
2024-01-29 17:46:31 +0900

    Feat : 프로필 페이지 도감 / 편집 구현

--- frontend/src/routes/ProfileCollection.tsx
+++ frontend/src/routes/ProfileCollection.tsx
@@ -1,57 +1,45 @@
 import Collection from "@/components/profile/Collection";
-import { useProfileDogamStore } from "@/stores/ProfileStore";
-import { useEffect } from "react";
+// import { useProfileDogamStore } from "@/stores/ProfileStore";
+// import { useEffect } from "react";
 
 export default function ProfileCollection() {
-  const { fetchProfileDogams, profileDogams } = useProfileDogamStore();
+  // const { fetchProfileDogams, profileDogams } = useProfileDogamStore();
 
-  useEffect(() => {
-    fetchProfileDogams();
-  }, [fetchProfileDogams]);
+  // useEffect(() => {
+  //   fetchProfileDogams();
+  // }, [fetchProfileDogams]);
 
   // 도감 갯수 임시 데이터
-  const data = [
+  const profileDogams = [
     {
-      title: "삽질하는 이수민머리ㅋㅋ",
-      bg: "/image/liveImage.png",
-      thumbsDown: false,
-      update: "2024.01.25",
+      dogamImg: "asdfasdf",
+      dogamTitle: "냠냠",
+      createdAt: "2020-01-01 00:00:00",
+      dogamHateAmount: 100,
+      isHated: false,
     },
     {
-      title: "삽질하는 삼수민머리ㅋㅋ",
-      bg: "/image/liveImage.png",
-      thumbsDown: false,
-      update: "2024.01.25",
+      dogamImg: "asdfasdf",
+      dogamTitle: "냠냠",
+      createdAt: "2020-01-01 00:00:00",
+      dogamHateAmount: 100,
+      isHated: false,
     },
     {
-      title: "삽질하는 사수민머리ㅋㅋ",
-      bg: "/image/liveImage.png",
-      thumbsDown: false,
-      update: "2024.01.25",
-    },
-    {
-      title: "삽질하는 오수민머리ㅋㅋ",
-      bg: "/image/liveImage.png",
-      thumbsDown: false,
-      update: "2024.01.25",
+      dogamImg: "asdfasdf",
+      dogamTitle: "냠냠",
+      createdAt: "2020-01-01 00:00:00",
+      dogamHateAmount: 100,
+      isHated: false,
     },
   ];
 
   return (
-    <div className="border-2 w-[1000px] m-1 grid grid-cols-3 row-auto place-items-center">
+    <div className="w-[1000px] m-1 grid grid-cols-3 row-auto place-items-center">
       {profileDogams &&
         Array.isArray(profileDogams) &&
         profileDogams.map((dogam, idx) => {
-          return (
-            <Collection
-              key={idx}
-              bg={dogam.dogamImg}
-              title={dogam.dogamTitle}
-              update={dogam.createdAt}
-              thumbsDown={dogam.isHated}
-              dogamHateAmount={dogam.dogamHateAmount}
-            />
-          );
+          return <Collection dogam={dogam} key={idx} />;
         })}
     </div>
   );
