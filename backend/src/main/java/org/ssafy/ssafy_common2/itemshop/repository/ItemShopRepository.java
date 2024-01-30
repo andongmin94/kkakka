4a56df7a8d0474a4c7036888208de81021b97661
oistmil <75559067+oistmil@users.noreply.github.com>
2024-01-30 13:25:53 +0900

    Feat : S10P12D110-199 아이템샵 조회

--- backend/src/main/java/org/ssafy/ssafy_common2/itemshop/repository/ItemShopRepository.java
+++ backend/src/main/java/org/ssafy/ssafy_common2/itemshop/repository/ItemShopRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.ssafy.ssafy_common2.itemshop.entity.Enforcement;
 import org.ssafy.ssafy_common2.itemshop.entity.ItemShop;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ItemShopRepository extends JpaRepository<ItemShop,Long> {
     Optional<ItemShop> findByItemNameAndDeletedAtIsNull(String type);
+    List<ItemShop> findAllByDeletedAtIsNull();
 }
