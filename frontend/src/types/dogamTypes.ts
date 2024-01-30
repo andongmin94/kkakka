02cb3fcf95243a85c128f04b61e19433fd5fd313
JIYEON KIM <jiyeon2536@naver.com>
2024-01-30 09:47:27 +0900

    fix: S10P12D110-181 메인페이지 에러수정

--- frontend/src/types/dogamTypes.ts
+++ frontend/src/types/dogamTypes.ts
@@ -1,20 +1,31 @@
-// 메인페이지 새로 올라온 도감
-export interface NewDogamType {
-  dogamOwnerName: string;
-  dogamOwnerAlias: string;
-  dogamOwnerProfilePic: string;
-  dogamPic: string;
-  dogamTitle: string;
-  dogamHateNumber: number;
-  isHated: boolean;
-  dogamRecentComment: string;
+// 도감에 달린 댓글
+export interface DogamCommentResponseType {
+  friendImgUrl: string | null;
+  userName: string | null;
+  userEmail: string | null;
+  comment: string | null;
 }
+
+// 도감 상세 (누르면 모달)
+export interface DogamDetailType {
+  dogamTitle: string;
+  dogamId: number;
+  friendName: string;
+  friendAlias: string | null;
+  dogamImgUrl: string;
+  friendImgUrl: string;
+  dogamDislikeNum: number;
+  dogamCommentResponseDto: DogamCommentResponseType;
+  hated: boolean;
+}
+
+// 메인페이지 새로 올라온 도감
+export type NewDogamType = Omit<DogamDetailType, "dogamCommentResponseDto"> & {
+  dogamRecentComment: string | null;
+};
 
 // 프로필에 있는 도감
 export type ProfileDogamType = Omit<
   NewDogamType,
-  | "dogamOwnerName"
-  | "dogamOwnerAlias"
-  | "dogamOwnerProfilePic"
-  | "dogamRecentComment"
+  "friendName" | "friendAlias" | "friendImgUrl" | "dogamRecentComment"
 >;
