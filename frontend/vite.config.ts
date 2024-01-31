4f53f2b5eb10c1716d90d1d0716718f30dce3505
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-02-01 00:53:20 +0900

    fix: Solve build issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import path from "path";
-import react from "@vitejs/plugin-react-swc";
-import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+// import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   base: "./",
@@ -9,68 +9,70 @@ export default defineConfig({
     port: 3000,
     host: "0.0.0.0",
   },
-  plugins: [react(), VitePWA({
-    registerType: "autoUpdate",
-    devOptions: {
-      enabled: true,
-    },
-    manifest: {
-      icons: [
-        {
-          src: "icons/icon-48x48.png",
-          sizes: "48x48",
-          type: "image/png",
-          purpose: "any maskable"
-        },
-        {
-          src: "icons/icon-72x72.png",
-          sizes: "72x72",
-          type: "image/png",
-          purpose: "any maskable"
-        },
-        {
-          src: "icons/icon-96x96.png",
-          sizes: "96x96",
-          type: "image/png",
-          purpose: "any maskable"
-        },
-        {
-          src: "icons/icon-128x128.png",
-          sizes: "128x128",
-          type: "image/png",
-          purpose: "any maskable"
-        },
-        {
-          src: "icons/icon-144x144.png",
-          sizes: "144x144",
-          type: "image/png",
-          purpose: "any maskable"
-        },
-        {
-          src: "icons/icon-152x152.png",
-          sizes: "152x152",
-          type: "image/png",
-          purpose: "any maskable"
-        },
-        {
-          src: "icons/icon-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-          purpose: "any maskable"
-        },
-        {
-          src: "icons/icon-384x384.png",
-          sizes: "384x384",
-          type: "image/png"
-        },
-        {
-          src: "icons/icon-512x512.png",
-          sizes: "512x512",
-          type: "image/png"
-        }
-      ],
-    },
-  })],
+  plugins: [react(),
+  //   VitePWA({
+  //   registerType: "autoUpdate",
+  //   devOptions: {
+  //     enabled: true,
+  //   },
+  //   manifest: {
+  //     icons: [
+  //       {
+  //         src: "icons/icon-48x48.png",
+  //         sizes: "48x48",
+  //         type: "image/png",
+  //         purpose: "any maskable"
+  //       },
+  //       {
+  //         src: "icons/icon-72x72.png",
+  //         sizes: "72x72",
+  //         type: "image/png",
+  //         purpose: "any maskable"
+  //       },
+  //       {
+  //         src: "icons/icon-96x96.png",
+  //         sizes: "96x96",
+  //         type: "image/png",
+  //         purpose: "any maskable"
+  //       },
+  //       {
+  //         src: "icons/icon-128x128.png",
+  //         sizes: "128x128",
+  //         type: "image/png",
+  //         purpose: "any maskable"
+  //       },
+  //       {
+  //         src: "icons/icon-144x144.png",
+  //         sizes: "144x144",
+  //         type: "image/png",
+  //         purpose: "any maskable"
+  //       },
+  //       {
+  //         src: "icons/icon-152x152.png",
+  //         sizes: "152x152",
+  //         type: "image/png",
+  //         purpose: "any maskable"
+  //       },
+  //       {
+  //         src: "icons/icon-192x192.png",
+  //         sizes: "192x192",
+  //         type: "image/png",
+  //         purpose: "any maskable"
+  //       },
+  //       {
+  //         src: "icons/icon-384x384.png",
+  //         sizes: "384x384",
+  //         type: "image/png"
+  //       },
+  //       {
+  //         src: "icons/icon-512x512.png",
+  //         sizes: "512x512",
+  //         type: "image/png"
+  //       }
+  //     ],
+  //   },
+  // })
+],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
