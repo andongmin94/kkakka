6531f4153dd8e19477998d40e1397566c2fe462e
Jeon Soo Min <wjsaos2081@gmail.com>
2024-01-31 09:05:27 +0900

    Feat/채팅 : 채팅 메세지 오고 가는 거 확인 및 채팅 칠 때마다 DB에 저장 완료
    Change-Id: I8ed2d9e79ffa5a5dcb52f9dded5c4f140dfd4b18
    
    Details:
    - Commit: 2c87a9ebdb722a9e981cce4c5ef1c2dea43d91ba
    - Author: Jeon Soo Min <wjsaos2081@gmail.com>
    - Date: 2024-01-31
    - Parent count: 1
    - Line changes: +149 / -19
    - Files:
    - M: backend/src/main/java/org/ssafy/ssafy_common2/chatting/config/SpringConfig.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/chatting/controller/ChatController.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/chatting/dto/request/ChatMessageDto.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/chatting/dto/response/ChatRoomInfoDto.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/chatting/entity/Message.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/chatting/repository/ChatJoinRepository.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/chatting/repository/ChatRoomRepository.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/chatting/repository/MessageRepository.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/chatting/service/ChatRoomMySQLService.java

--- backend/src/main/java/org/ssafy/ssafy_common2/chatting/repository/MessageRepository.java
+++ backend/src/main/java/org/ssafy/ssafy_common2/chatting/repository/MessageRepository.java
@@ -1,8 +1,17 @@
 package org.ssafy.ssafy_common2.chatting.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import org.ssafy.ssafy_common2.chatting.entity.Message;
-
+@Transactional
 public interface MessageRepository extends JpaRepository<Message,Long> {
 
+    @Modifying
+    @Query(value = "insert into message(content, message_type, user_id, chat_room_id ) " +
+            "values (:content, :message_type, :user_id, :chat_room_id )",nativeQuery = true)
+    void InsertMessage( @Param("content") String content, @Param("message_type") String message_type,
+                       @Param("user_id") long user_id, @Param("chat_room_id") long chat_room_id);
 }
