63a99f454d9f20919beb13b7309f23125cec959d
김상훈 <k1016h@naver.com>
2024-02-01 17:56:37 +0900

    Feat : electron root to src

--- frontend/src/electron/main.js
+++ frontend/src/electron/main.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import { join, dirname } from "path";
 import electronLocalshortcut from "electron-localshortcut";
+import { createWebSocketConnection } from "league-connect";
 import { app, ipcMain, BrowserWindow, Tray, Menu, nativeImage } from "electron";
 
 dotenv.config();
@@ -72,9 +73,11 @@ ipcMain.on("minimize", (event) => {
 
 ipcMain.on("maximize", (event) => {
   if (win.isMaximized()) {
-    win.restore();
+    win.setBounds({ width: 800, height: 600 });
+    console.log("restoring");
   } else {
     win.maximize();
+    console.log("maximizing");
   }
 });
 
@@ -83,16 +86,28 @@ ipcMain.on("hidden", (event) => {
 });
 
 //////////////////////////////////////////////////////////
+// 리액트 통신
 
 ipcMain.on("button-clicked", (event, message) => {
-  console.log("Received from button-clicked : ", message);
-  win.webContents.send("receive-from-electron", message);
+  console.log("메인 프로세스에서 받은 메시지 : ", message);
+  win.webContents.send("channel-name", message);
 });
 
 ipcMain.on("Riot Game Info", (event, message) => {
   console.log("Received from Riot Game Info : ", message);
 });
 
-ipcMain.on("lol", (event, message) => {
-  console.log("actions : ", message);
+//////////////////////////////////////////////////////////
+// 리그오브레전드 통신
+
+const ws = await createWebSocketConnection({
+  authenticationOptions: {
+    awaitConnection: true,
+  },
+});
+
+ws.subscribe("/lol-champ-select/v1/session", (data) => {
+  const champId = data.actions[0][0].championId;
+  // const champId = data.actions;
+  console.log(champId);
 });
