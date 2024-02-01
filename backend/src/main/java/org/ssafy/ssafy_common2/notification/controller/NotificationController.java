e529ed4923de2db58f1bfd4623fc5ac827942076
oistmil <oistmil@gmail.com>
2024-02-01 09:30:09 +0900

    Feat : S10P12D110-202 알림 리스트, 알림 확인 기능
    Details:
    - Commit: 0ea1f4f359d0b0eea44f92387138059abab917cb
    - Author: oistmil <oistmil@gmail.com>
    - Date: 2024-02-01
    - Parent count: 1
    - Line changes: +133 / -11
    - Files:
    - M: backend/src/main/java/org/ssafy/ssafy_common2/notification/controller/NotificationController.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/notification/dto/response/NotificationListResponseDto.java
    - A: backend/src/main/java/org/ssafy/ssafy_common2/notification/dto/response/UpdateNotificationResponseDto.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/notification/entity/Notification.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/notification/repository/EmitterRepository.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/notification/repository/EmitterRepositoryImpl.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/notification/repository/NotificationRepository.java
    - M: backend/src/main/java/org/ssafy/ssafy_common2/notification/service/NotificationService.java

--- backend/src/main/java/org/ssafy/ssafy_common2/notification/controller/NotificationController.java
+++ backend/src/main/java/org/ssafy/ssafy_common2/notification/controller/NotificationController.java
@@ -2,12 +2,14 @@ package org.ssafy.ssafy_common2.notification.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.ssafy.ssafy_common2._common.response.ApiResponseDto;
+import org.ssafy.ssafy_common2._common.response.MsgType;
+import org.ssafy.ssafy_common2._common.response.ResponseUtils;
 import org.ssafy.ssafy_common2._common.security.UserDetailsImpl;
+import org.ssafy.ssafy_common2.notification.dto.response.NotificationListResponseDto;
+import org.ssafy.ssafy_common2.notification.dto.response.UpdateNotificationResponseDto;
 import org.ssafy.ssafy_common2.notification.service.NotificationService;
 
 @RestController
@@ -22,6 +24,20 @@ public class NotificationController {
                                 @RequestHeader(value="Last-Event-Id", required = false, defaultValue = "") String lastEventId) {
 
         return notificationService.subscribe(userDetails.getUsername(), lastEventId);
-
     }
+
+    @GetMapping("/alarm")
+    public ApiResponseDto<NotificationListResponseDto> getAlarmAll(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        return ResponseUtils.ok(notificationService.getNoficiationList(userDetails.getUser()), MsgType.SEARCH_SUCCESSFULLY);
+    }
+
+    @PutMapping("/alarm/{alarm-id}")
+    public ApiResponseDto<UpdateNotificationResponseDto> readAlarm(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                                   @PathVariable("alarm-id") Long alarmId) {
+
+        return ResponseUtils.ok(notificationService.readAlarm(alarmId, userDetails.getUser()), MsgType.UPDATE_ALARM_SUCCESSFULLY);
+    }
+
+
 }
