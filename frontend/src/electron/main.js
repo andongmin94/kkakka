9596c2f7d267f162d17d67afa6ba5d815a938555
김상훈 <k1016h@naver.com>
2024-02-02 17:48:18 +0900

    Feat : electron parsing
    Details:
    - Commit: 32aee90c49abaab3fe78d2644ca659be2bd0a413
    - Author: 김상훈 <k1016h@naver.com>
    - Date: 2024-02-02
    - Parent count: 1
    - Line changes: +78 / -85
    - Files:
    - M: frontend/src/electron/SettingPage.tsx
    - M: frontend/src/electron/TitleBar.tsx
    - M: frontend/src/electron/main.js
    - D: frontend/src/electron/test.js

--- frontend/src/electron/main.js
+++ frontend/src/electron/main.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+import https from "https";
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import { join, dirname } from "path";
@@ -31,6 +33,36 @@ function createWindow() {
     console.log("F12 is pressed");
     win.webContents.toggleDevTools();
   });
+
+  ipcMain.on("minimize", (event) => {
+    win.minimize();
+  });
+
+  ipcMain.on("maximize", (event) => {
+    if (win.isMaximized()) {
+      win.setBounds({ width: 800, height: 600 });
+      console.log("restoring");
+    } else {
+      win.maximize();
+      console.log("maximizing");
+    }
+  });
+
+  ipcMain.on("hidden", (event) => {
+    win.hide();
+  });
+
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+  });
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 }
 
 // 이 메소드는 Electron의 초기화가 완료되고
@@ -54,57 +86,54 @@ app
       win.show();
     });
 
-//////////////////////////////////////////////////////////
-// 리그오브레전드 통신
-
+    //////////////////// 리그오브레전드 통신 /////////////////////
     const ws = await createWebSocketConnection({
       authenticationOptions: {
         awaitConnection: true,
       },
     });
     
-    ws.subscribe("/lol-champ-select/v1/session", (data) => {
-      const champId = data.actions[0][0].championId;
-      // const champId = data.actions;
-      console.log(champId);
+    ws.subscribe('/lol-gameflow/v1/session', (data) => {
+      if (data.phase === 'GameStart')
+      {
+        console.log('게임이 시작되었습니다!')
+        async function fetchEventData() {
+          try {
+            const response = await axios({
+              method: "get",
+              url: "https://127.0.0.1:2999/liveclientdata/allgamedata",
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+              },
+              httpsAgent: new https.Agent({
+                rejectUnauthorized: false,
+              }),
+            });
+            console.log(response.data);
+          } catch (error) {
+            console.error(error);
+          }
+        }
+        function loopFunction() {
+          fetchEventData();
+          
+          // 1초 후에 loopFunction을 다시 호출합니다.
+          setTimeout(loopFunction, 1000);
+        }
+        // 함수를 최초로 호출하여 루프를 시작합니다.
+        loopFunction();
+      }
+      if (data.phase === 'WaitingForStats')
+      { 
+        console.log('게임이 종료되었습니다!')
+        return;
+      }
     });
-    
-//////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////
   });
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
-
-app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-
-ipcMain.on("minimize", (event) => {
-  win.minimize();
-});
-
-ipcMain.on("maximize", (event) => {
-  if (win.isMaximized()) {
-    win.setBounds({ width: 800, height: 600 });
-    console.log("restoring");
-  } else {
-    win.maximize();
-    console.log("maximizing");
-  }
-});
-
-ipcMain.on("hidden", (event) => {
-  win.hide();
-});
-
-//////////////////////////////////////////////////////////
-// 리액트 통신
-
+////////////////////// 리액트 통신 //////////////////////////
 ipcMain.on("button-clicked", (event, message) => {
   console.log("메인 프로세스에서 받은 메시지 : ", message);
   win.webContents.send("channel-name", message);
@@ -113,7 +142,4 @@ ipcMain.on("button-clicked", (event, message) => {
 ipcMain.on("Riot Game Info", (event, message) => {
   console.log("Received from Riot Game Info : ", message);
 });
-
-
-
-
+////////////////////////////////////////////////////////////
