5eb0763ada5cf99a8d2b8da6649558ea99028acf
오세영 <76714304+osy9536@users.noreply.github.com>
2024-02-03 22:52:17 +0900

    Fix : 본인 유저 데이터 조회

--- backend/src/main/java/org/ssafy/ssafy_common2/user/repository/DynamicUserInfoRepository.java
+++ backend/src/main/java/org/ssafy/ssafy_common2/user/repository/DynamicUserInfoRepository.java
@@ -8,6 +8,7 @@ import org.ssafy.ssafy_common2.user.entity.DynamicUserInfo;
 import java.util.Optional;
 
 public interface DynamicUserInfoRepository extends JpaRepository<DynamicUserInfo,Long> {
+
     Optional<DynamicUserInfo> findByIdAndDeletedAtIsNull(Long id);
     @Query(value = "UPDATE dynamic_user_info dui SET dui.point = :betPoint WHERE dui.id = :id", nativeQuery = true)
     void UpdateUserBettingPoint(int betPoint, long id);
