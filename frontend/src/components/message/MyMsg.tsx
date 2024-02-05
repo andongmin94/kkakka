75234658ad9b12b74cab1c0b6914d19f14910e78
lhgeer2617 <lhgeer2617@gmail.com>
2024-02-06 01:33:43 +0900

    Feat : 1:1 채팅 구현(도감 선택 제외)
    Feat : 1:1 채팅 구현(도감 선택 제외)

--- frontend/src/components/message/MyMsg.tsx
+++ frontend/src/components/message/MyMsg.tsx
@@ -1,19 +1,25 @@
+import { Mobile, PC } from "../MediaQuery";
+
 interface dataProps {
   userId: string;
   name: string;
-  type: string;
+  msgUserProfile: string;
+  img: any; // 아무리 해도 해결이 안돼서 일단 any
   content: string;
 }
 
 export default function MyMsg({ data }: { data: dataProps }) {
   return (
-    <div className="max-w-[60%] m-2 p-3 rounded-2xl self-end bg-orange-300 text-white font-bold text-xl text-wrap">
-      {/* 사진인지 텍스트인지 확인 */}
-      {data.type === "img" ? (
-        <img src={data.content} className=" rounded-2xl" />
-      ) : (
-        data.content
-      )}
-    </div>
+    <>
+      <PC>
+        <div className="max-w-[70%] m-2 p-3 rounded-2xl self-end bg-orange-300 text-white font-bold text-xl text-wrap">
+          {/* 이미지인지 텍스트 채팅인지 판별 */}
+          {data.img ? <img src={data.img} /> : data.content}
+        </div>
+      </PC>
+      <Mobile>
+        <></>
+      </Mobile>
+    </>
   );
 }
