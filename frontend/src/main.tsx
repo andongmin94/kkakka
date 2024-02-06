0842f39a4cd261d3e1487ff12a058b39d1b5092d
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-02-07 03:12:12 +0900

    Feat/도감삭제완성,Message Entity 수정(Chat bot)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,6 +17,7 @@ import ProfileDishonorPage from "@/routes/ProfileDishonorPage";
 import { ThemeProvider } from "@/components/navbar/ThemeProvider";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Chat from './routes/Chat.jsx';
 ////////////////일렉트론 컴포넌트/////////////////////
 const electron = window.electron;
 import TitleBar from "@/electron/TitleBar.tsx";
