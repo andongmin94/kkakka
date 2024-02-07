ac7d6e2467de4e6706d37d9a7cb899d7555b5f53
김상훈 <k1016h@naver.com>
2024-02-07 17:56:53 +0900

    Fix : electron fix
    Details:
    - Commit: 87d9560f8ae4927648901ffdc28f3f20f55876a7
    - Author: 김상훈 <k1016h@naver.com>
    - Date: 2024-02-07
    - Parent count: 1
    - Line changes: +270 / -98
    - Files:
    - M: frontend/src/electron/SettingPage.tsx
    - M: frontend/src/electron/main.js

--- frontend/src/electron/SettingPage.tsx
+++ frontend/src/electron/SettingPage.tsx
@@ -2,9 +2,17 @@ import axios from "axios";
 const electron = window.electron;
 import { Slider } from "@/components/ui/slider";
 
-// electron.on('channel-name', () => { console.log("리액트에서 받았음") });
-
 export default function SettingPage() {
+
+  /// 환경 체크
+  if (typeof electron !== "undefined") {
+    // 일렉트론 환경
+    console.log("일렉트론 애플리케이션에서 실행 중입니다.");
+  } else {
+    // 브라우저 환경
+    console.log("웹 브라우저에서 실행 중입니다.");
+  }
+
   const handleClick = () => {
     electron.send("button-clicked", "리액트에서 보냄");
   };
@@ -19,25 +27,33 @@ export default function SettingPage() {
     console.log(response.data);
   };
 
-  const handleTransparencyChange = (value: any = 100) => {
-    // 투명도 값이 변경될 때의 로직을 여기에 추가
-    document.body.style.backgroundColor = `rgba(255, 255, 255, ${1 - value / 100})`;
-  };
-
-  /// 환경 체크
-  if (typeof electron !== "undefined") {
-    // 일렉트론 환경
-    console.log("일렉트론 애플리케이션에서 실행 중입니다.");
-  } else {
-    // 브라우저 환경
-    console.log("웹 브라우저에서 실행 중입니다.");
+  function notifyMe() {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+  
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      var notification = new Notification("Hi there!");
+    }
+  
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+      Notification.requestPermission(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          var notification = new Notification("Hi there!");
+        }
+      });
+    }
+  
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them any more.
   }
-
   return (
     <div>
-      <div>
-        <Slider onValueChange={handleTransparencyChange} className="w-40" />
-      </div>
       <br />
       <br />
       <button onClick={handleClick}>Click Me</button>
@@ -45,6 +61,10 @@ export default function SettingPage() {
       <br />
       <button onClick={summonerName}>Click Me</button>
       <br />
+      <br />
+      <button onClick={notifyMe}>Notify me!</button>
+      <br />
     </div>
   );
 }
+
