02587bb33f598128747cedafe6c4423cf9738ef0
JIYEON KIM <jiyeon2536@naver.com>
2024-02-08 05:37:49 +0900

    refactor : 리팩토링 유저 포인트 조회

--- frontend/src/hooks/dm/mutations/useEnterDmPost.ts
+++ frontend/src/hooks/dm/mutations/useEnterDmPost.ts
@@ -1,9 +1,15 @@
 import { enterDmPost } from "@/services/dm/enterDmPostApi";
 import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 
 export const useEnterDmPost = () => {
   const mutation = useMutation({
     mutationFn: (data: string) => enterDmPost(data),
+    onSuccess: (res) => {
+      console.log("dm 보내기", res);
+      const navigate = useNavigate();
+      navigate(`/main/message/${res}`);
+    },
   });
 
   return mutation;
