d8897edaead93c21f22b72f0f96bc1270ddf58e3
오세영 <76714304+osy9536@users.noreply.github.com>
2024-02-08 13:59:00 +0900

    Feat : S10P12D110-168 프로필 도감 리스트 구현

--- backend/src/main/java/org/ssafy/ssafy_common2/dogam/repository/DislikeDogamRepository.java
+++ backend/src/main/java/org/ssafy/ssafy_common2/dogam/repository/DislikeDogamRepository.java
@@ -3,6 +3,7 @@ package org.ssafy.ssafy_common2.dogam.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.ssafy.ssafy_common2.dogam.entity.DislikeDogam;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DislikeDogamRepository extends JpaRepository<DislikeDogam,Long> {
@@ -11,4 +12,6 @@ public interface DislikeDogamRepository extends JpaRepository<DislikeDogam,Long>
     Optional<DislikeDogam> findByUserEmailAndDogamIdAndDeletedAtIsNull(String kakaoEmail, Long id);
 
     Optional<DislikeDogam> findByIdAndDeletedAtIsNull(Long dislikeId);
+
+    List<DislikeDogam> findAllByDogamIdAndDeletedAtIsNull(Long dogamId);
 }
