4d6c306a674173de5edd582b2acf9d41161af201
JIYEON KIM <jiyeon2536@naver.com>
2024-02-09 10:00:21 +0900

    refactor : 내프로필 페이지와 다른 프로필페이지 분리

--- frontend/src/routes/ProfilePage.tsx
+++ frontend/src/routes/ProfilePage.tsx
@@ -3,12 +3,10 @@ import Poor from "@/components/profile/Poor";
 import classes from "./ProfilePage.module.css";
 import { Button } from "@/components/ui/button";
 import { Mobile, PC } from "@/components/MediaQuery";
-import ProfileEdit from "@/components/profile/ProfileEdit";
 import { Link, Outlet, useParams } from "react-router-dom";
 import ProfileImage from "@/components/profile/ProfileImage";
 import UserCurrentAlias from "@/components/UserCurrentAlias";
 import { useEffect } from "react";
-import useUserStore from "@/store/user/userStore";
 import useProfileStore from "@/store/profile/profileStore";
 import useFriendshipStore from "@/store/profile/friendshipStore";
 import { useProfile } from "@/hooks/profile/queries/useUserProfileQuery";
@@ -18,7 +16,7 @@ import { useFriendshipChangePost } from "@/hooks/profile/mutations/useFriendship
 
 export default function ProfilePage() {
   const params = useParams();
-  const { userInfo } = useUserStore();
+
   const { profileInfo, setProfileInfo } = useProfileStore();
   const { friendship, setFriendship } = useFriendshipStore();
 
@@ -36,7 +34,6 @@ export default function ProfilePage() {
       console.log("프로필 유저 정보 없음");
     }
   }, [userProfileData, setProfileInfo]);
-
   const enterDmMutation = useEnterDmPost();
   const { mutate: dmMutate } = enterDmMutation;
 
@@ -65,7 +62,6 @@ export default function ProfilePage() {
       <PC>
         <div className="w-full flex flex-col items-center">
           {/* 프로필 배경 */}
-          {/* 동적으로 주면 자꾸 됐다가 안됐다가 해서 일단 직접 입력으로 넣음 */}
           <div className="border-8 rounded-2xl border-red-200">
             <div
               className={`border-4 w-[1000px] h-[350px] flex-col m-1 border-blue-200 bg-[url("/image/profileBg.png")] bg-cover rounded-2xl`}
@@ -73,61 +69,52 @@ export default function ProfilePage() {
               <div className="flex justify-between m-1">
                 <div className="m-1 w-[550px] h-[220px]">
                   <div className="m-1 w-[200px] h-[200px] grid place-items-center">
-                    {/* 프사 */}
                     <ProfileImage userImg={profileInfo.userProfileImg} />
                   </div>
                 </div>
                 <div className="m-1 w-[550px] h-[200px] flex justify-end">
                   <div className="pt-[12px] mr-2">
-                    {/* 뭔지 정확하게 몰라서 일단 체크 버튼 만들었음 */}
                     {/* <Check check={check} /> */}
                   </div>
-                  {/* 파산 이미지 */}
                   {profileInfo.bankruptcy ? <Poor /> : null}
-
-                  {/* 프로필 편집 or 메세지 버튼 */}
-                  {profileInfo.userId === userInfo.userId ? (
-                    <ProfileEdit />
-                  ) : (
-                    <>
-                      <Button
-                        type="submit"
-                        variant="secondary"
-                        className="mr-1 border-solid border-2 border-inherit bg-white font-bold text-lg mt-2 h-[50px]"
-                        onClick={() => {
-                          enterChatHandler();
-                        }}
-                      >
-                        메세지
+                  <div>
+                    <Button
+                      type="submit"
+                      variant="secondary"
+                      className="mr-1 border-solid border-2 border-inherit bg-white font-bold text-lg mt-2 h-[50px]"
+                      onClick={() => {
+                        enterChatHandler();
+                      }}
+                    >
+                      메세지
+                    </Button>
+                    {(friendship === "FRIEND" && (
+                      <Button onClick={changeFriendStatusHandler}>
+                        친구끊기
                       </Button>
-                      {(friendship === "FRIEND" && (
+                    )) ||
+                      (friendship === "RECEIVE" && (
                         <Button onClick={changeFriendStatusHandler}>
-                          친구끊기
+                          수락
                         </Button>
                       )) ||
-                        (friendship === "RECEIVE" && (
-                          <Button onClick={changeFriendStatusHandler}>
-                            수락
-                          </Button>
-                        )) ||
-                        (friendship === "SEND" && (
-                          <Button onClick={changeFriendStatusHandler}>
-                            요청취소
-                          </Button>
-                        )) ||
-                        (friendship === "NONE" && (
-                          <Button onClick={changeFriendStatusHandler}>
-                            친구신청
-                          </Button>
-                        ))}
-                    </>
-                  )}
+                      (friendship === "SEND" && (
+                        <Button onClick={changeFriendStatusHandler}>
+                          요청취소
+                        </Button>
+                      )) ||
+                      (friendship === "NONE" && (
+                        <Button onClick={changeFriendStatusHandler}>
+                          친구신청
+                        </Button>
+                      ))}
+                  </div>
                 </div>
               </div>
               <div className="m-1 w-100% h-[100px] flex gap-[100px] items-center pl-[35px]">
                 <div className="font-bold text-4xl">
                   <div className="bg-white text-black rounded-2xl border-4 border-red-300 w-[150px] h-[60px] grid grid-col place-items-center">
-                    {profileInfo && profileInfo.userName}
+                    {profileInfo.userName}
                   </div>
                 </div>
                 <div>
@@ -145,7 +132,6 @@ export default function ProfilePage() {
                 <Link to={`/main/profile/${params.id}/dishonor`}>
                   불명예 전당
                 </Link>
-                <Link to={`/main/profile/${params.id}/record`}>전적</Link>
               </div>
             </div>
 
@@ -159,7 +145,6 @@ export default function ProfilePage() {
       <Mobile>
         <div className="w-full flex flex-col items-center">
           {/* 프로필 배경 */}
-          {/* 동적으로 주면 자꾸 됐다가 안됐다가 해서 일단 직접 입력으로 넣음 */}
           <div className="border-8 rounded-2xl w-full border-red-200 flex flex-col">
             <div
               className={`border-4 w-full h-[350px] flex-col border-blue-200 bg-[url("/image/profileBg.png")] bg-cover rounded-2xl`}
@@ -167,53 +152,43 @@ export default function ProfilePage() {
               <div className="flex justify-between m-1">
                 <div className="m-1 w-[550px] h-[220px]">
                   <div className="m-1 w-[150px] h-[150px] grid place-items-center">
-                    {/* 프사 */}
                     <ProfileImage userImg={profileInfo.userProfileImg} />
                   </div>
                 </div>
                 <div className="m-1 w-[550px] h-[200px] flex justify-end">
                   <div className="pt-[12px] mr-2">
-                    {/* 뭔지 정확하게 몰라서 일단 체크 버튼 만들었음 */}
                     {/* <Check check={check} /> */}
                   </div>
-                  {/* 파산 이미지 */}
-                  {/* 파산일때만 보이게 */}
                   {profileInfo.bankruptcy ? <Poor /> : null}
-
-                  {/* 프로필 편집 or 메세지 버튼 */}
-                  {profileInfo.userId === userInfo.userId ? (
-                    <ProfileEdit />
-                  ) : (
-                    <>
-                      <Button
-                        type="submit"
-                        variant="secondary"
-                        className="mr-1 border-solid border-2 border-inherit bg-white font-bold text-lg mt-2 h-[50px]"
-                      >
-                        메세지
+                  <div>
+                    <Button
+                      type="submit"
+                      variant="secondary"
+                      className="mr-1 border-solid border-2 border-inherit bg-white font-bold text-lg mt-2 h-[50px]"
+                    >
+                      메세지
+                    </Button>
+                    {(friendship === "FRIEND" && (
+                      <Button onClick={changeFriendStatusHandler}>
+                        친구끊기
                       </Button>
-                      {(friendship === "FRIEND" && (
+                    )) ||
+                      (friendship === "RECEIVE" && (
                         <Button onClick={changeFriendStatusHandler}>
-                          친구끊기
+                          수락
                         </Button>
                       )) ||
-                        (friendship === "RECEIVE" && (
-                          <Button onClick={changeFriendStatusHandler}>
-                            수락
-                          </Button>
-                        )) ||
-                        (friendship === "SEND" && (
-                          <Button onClick={changeFriendStatusHandler}>
-                            요청취소
-                          </Button>
-                        )) ||
-                        (friendship === "NONE" && (
-                          <Button onClick={changeFriendStatusHandler}>
-                            친구신청
-                          </Button>
-                        ))}
-                    </>
-                  )}
+                      (friendship === "SEND" && (
+                        <Button onClick={changeFriendStatusHandler}>
+                          요청취소
+                        </Button>
+                      )) ||
+                      (friendship === "NONE" && (
+                        <Button onClick={changeFriendStatusHandler}>
+                          친구신청
+                        </Button>
+                      ))}
+                  </div>
                 </div>
               </div>
               <div className="m-1 w-100% h-[100px] flex gap-[100px] items-center pl-[5px] justify-between">
@@ -240,9 +215,6 @@ export default function ProfilePage() {
                   </Link>
                   <Link to={`/main/profile/${profileInfo.userId}/dishonor`}>
                     불명예 전당
-                  </Link>
-                  <Link to={`/main/profile/${profileInfo.userId}/record`}>
-                    전적
                   </Link>
                 </div>
               </div>
