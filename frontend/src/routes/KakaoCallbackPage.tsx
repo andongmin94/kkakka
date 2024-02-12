64b0b6069f84a12ff59be7ec918fa8ba36e75572
오세영 <osy9536@naver.com>
2024-02-12 23:33:07 +0900

    Test : 배포 환경 테스트를 위한 env 파일 변경
    Details:
    - Commit: bdbb092dfecde97820c71b92e9ea522f9c26ce6b
    - Author: 오세영 <osy9536@naver.com>
    - Date: 2024-02-12
    - Parent count: 1
    - Line changes: +1 / -1
    - Files:
    - M: frontend/src/routes/KakaoCallbackPage.tsx

--- frontend/src/routes/KakaoCallbackPage.tsx
+++ frontend/src/routes/KakaoCallbackPage.tsx
@@ -14,7 +14,7 @@ export default function KakaoCallbackPage() {
         .get(
           `${
             import.meta.env.VITE_API_BASE_URL
-          }/api/oauth/callback/kakao/token/l-t-l?code=${code}`
+          }/api/oauth/callback/kakao/token/d-t-d?code=${code}`
         )
         .then((res) => {
           localStorage.setItem("token", res.headers.authorization);
