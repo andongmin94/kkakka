a556caa7e5cd250c6a557faf0d77848342a9895a
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-02-13 00:43:24 +0900

    Refactor: 캐로셀 ui 수정

--- frontend/src/routes/IndexPage.tsx
+++ frontend/src/routes/IndexPage.tsx
@@ -16,10 +16,10 @@ export default function IndexPage() {
           <div className="text-lg flex">
             <p className="grid font-bold ml-10">라이브</p>
           </div>
-          <div className="h-[100px] w-[1200px] flex flex-col items-center">
+          <div className="h-[100px] flex flex-col items-center">
             <LiveContentCarousel />
           </div>
-          <div className="text-lg mt-[350px] mb-5 flex ">
+          <div className="text-lg mt-[300px] mb-5 flex ">
             <p className="grid  ml-10 font-bold">새로 올라온 도감</p>
           </div>
           <div className="grid grid-cols-2 gap-y-5 mb-5 w-[1050px] ml-10">
