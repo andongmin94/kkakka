7d7406c3f720ccf4d3d9bb835637d7f405cbfe2d
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-02-13 01:25:51 +0900

    Refactor : 라이브 콘텐츠 ui 수정

--- frontend/src/components/main/LiveProfile.tsx
+++ frontend/src/components/main/LiveProfile.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import classes from "./LiveProfile.module.css";
 
 interface LiveProfileProps {
   playerName: string;
@@ -14,19 +15,24 @@ function Skeleton({
 }
 
 export default function LiveProfile({
-  // playerName,
+  playerName,
   roomTitle,
   playerProfilePic,
 }: LiveProfileProps) {
   return (
     <div>
-      <Skeleton
-        className={`w-[80px] h-[80px] rounded-full border-4  border-red-500 `}
-        style={{
-          backgroundImage: `url("${playerProfilePic}")`,
-          backgroundSize: "cover",
-        }}
-      />
+      <div className="flex items-center">
+        <Skeleton
+          className={`w-[80px] h-[80px] rounded-full border-4 ${classes.animatedBorder} shadow-md mr-2`}
+          style={{
+            backgroundImage: `url("${playerProfilePic}")`,
+            backgroundSize: "cover",
+          }}
+        />
+        <div className=" w-fit rounded-md text-xs bg-opacity-50 text-white p-1 mt-14">
+          {playerName} playing
+        </div>
+      </div>
       <div className="space-x-3">
         <div className="space-y-2">
           <Skeleton className="h-[60px] grid grid-rows-1 place-items-stretch mt-5">
