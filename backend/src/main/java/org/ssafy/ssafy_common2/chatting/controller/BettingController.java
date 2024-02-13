fefabfe1247e87287f229361d5e65739608662d3
Jeon Soo Min <102154788+dalcheonroadhead@users.noreply.github.com>
2024-02-13 14:53:59 +0900

    Feat/배팅: 배팅 구현

--- backend/src/main/java/org/ssafy/ssafy_common2/chatting/controller/BettingController.java
+++ backend/src/main/java/org/ssafy/ssafy_common2/chatting/controller/BettingController.java
@@ -4,9 +4,7 @@ import io.lettuce.core.dynamic.annotation.Param;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.ssafy.ssafy_common2._common.exception.ErrorResponse;
 import org.ssafy.ssafy_common2._common.exception.ErrorType;
 import org.ssafy.ssafy_common2._common.response.ApiResponseDto;
@@ -15,9 +13,15 @@ import org.ssafy.ssafy_common2._common.response.ResponseUtils;
 import org.ssafy.ssafy_common2._common.security.UserDetailsImpl;
 import org.ssafy.ssafy_common2.chatting.dto.response.BettingDto;
 import org.ssafy.ssafy_common2.chatting.dto.response.DividendsDto;
+import org.ssafy.ssafy_common2.chatting.dto.response.PredictDto;
+import org.ssafy.ssafy_common2.chatting.entity.ChatRoom;
+import org.ssafy.ssafy_common2.chatting.entity.Message;
 import org.ssafy.ssafy_common2.chatting.repository.ChatJoinRepository;
+import org.ssafy.ssafy_common2.chatting.repository.ChatRoomRepository;
 import org.ssafy.ssafy_common2.chatting.service.BettingService;
 import org.ssafy.ssafy_common2.chatting.service.ChatRoomMySQLService;
+import org.ssafy.ssafy_common2.user.entity.User;
+import org.ssafy.ssafy_common2.user.repository.UserRepository;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -26,19 +30,19 @@ import org.ssafy.ssafy_common2.chatting.service.ChatRoomMySQLService;
 public class BettingController {
 
     private final BettingService bettingService;
-    private final ChatRoomMySQLService chatRoomMySQLService;
-    private final ChatJoinRepository chatJoinRepository;
+    private final ChatRoomRepository chatRoomRepository;
+    private final UserRepository userRepository;
 
 
     // 1) 배팅하기
-    @PostMapping("/{roomId}")
+    @PostMapping("/{room_id}")
     public ApiResponseDto<?> LetsBetting(
-            @AuthenticationPrincipal UserDetailsImpl userDetails,
-            @Param(value = "room_id") long room_id,
-            @Param(value = "cur_betting_point") int cur_betting_point,
+            @PathVariable(value = "room_id") long room_id,
+            @Param(value = "user_id") long user_id,
+            @Param(value = "cur_betting_point") Integer cur_betting_point,
             @Param(value = "is_win") boolean is_win) {
 
-        BettingDto ans = bettingService.LetsBetting(userDetails.getUser().getId()
+        BettingDto ans = bettingService.LetsBetting( user_id
         , room_id,cur_betting_point, is_win);
 
         if(ans == null){
@@ -49,10 +53,10 @@ public class BettingController {
     }
 
     // 2) 배팅 금액 정산 받기
-    @PostMapping("/{roomId}/result")
+    @PostMapping("/{room_id}/result")
     public ApiResponseDto<?> GetBettingResult(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
-            @Param(value = "room_id")long room_id,
+            @PathVariable(value = "room_id")long room_id,
             @Param(value = "is_win") boolean is_win){
 
         DividendsDto dto;
@@ -65,5 +69,37 @@ public class BettingController {
         }
 
         return ResponseUtils.ok(dto,MsgType.DATA_SUCCESSFULLY);
+    }
+
+    // 3) 현재 총 배팅 현황을 확인
+    @GetMapping("room/{room_id}")
+    public ApiResponseDto<?> GetTotalBettingResult(
+            @PathVariable(value = "room_id") long room_id
+    ) {
+        ChatRoom chatRoom = chatRoomRepository.findById(room_id).orElse(null);
+
+        PredictDto predictDto = new PredictDto();
+
+        if(chatRoom == null){
+            return ResponseUtils.error(ErrorResponse.of(ErrorType.NO_LIVE_BROADCAST_ROOM));
+        }
+
+        predictDto.setPredictWin(chatRoom.getWinPoint());
+        predictDto.setPredictLose(chatRoom.getLosePoint());
+
+        return ResponseUtils.ok(predictDto, MsgType.DATA_SUCCESSFULLY);
+    }
+
+    @GetMapping("user/{user_id}")
+    public ApiResponseDto<?> getUserBettingBalance(
+            @PathVariable(value = "user_id")long user_id
+    ) {
+       User user = userRepository.findById(user_id).orElse(null);
+
+       if(user == null){
+           return ResponseUtils.error(ErrorResponse.of(ErrorType.NOT_FOUND_USER));
+       }
+
+       return ResponseUtils.ok(user.getUserInfoId().getPoint(), MsgType.DATA_SUCCESSFULLY);
     }
 }
--- frontend/src/routes/LiveChat.tsx
+++ frontend/src/routes/LiveChat.tsx
@@ -21,9 +21,31 @@ import {
   DialogTrigger,
   DialogClose,
 } from "@/components/ui/dialog";
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+
+
+import { useToast } from "@/components/ui/use-toast"
+import { Toaster } from "@/components/ui/toaster"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { Progress } from "@/components/ui/progress"
+import {  MinusIcon, PlusIcon } from "@radix-ui/react-icons"
+import { Bar, BarChart, ResponsiveContainer, Cell, XAxis } from "recharts"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import SysMsg from "@/components/message/SysMsg";
 import { DogamDetailType } from "@/types/dogamTypes";
+import { BettingPredictType } from "@/types/BettingTypes";
 import BotMsg from "@/components/message/BotMsg";
+
 
 let stompClient: any;
 
@@ -60,31 +82,91 @@ export default function LiveChat() {
   const [messages, setMessages] = useState<any[]>([]);
   // 현재 다른 사람이 타이핑하는 메세지를 추적
   const [currentTypingId, setCurrentTypingId] = useState(null);
-  // 현재 사용자가 업로드한 이미지
-  // const [curImg, setImgFile] = useState("");
-  // const imgRef = useRef();
 
   // 도감 전체 목록
   const [dogamList, setDogamList] = useState<DogamDetailType[]>([]);
+  
+  const [predictObject, setPredictObject] = useState<BettingPredictType>({
+    predictWin: 100,
+    predictLose: 100
+  });
 
-  // const handleSendMessage = (message: any) => {
-  //   console.log(message);
-  //   // 소켓으로 메세지 보내기
-  //   sendMessageToSocket(message);
-  // };
+  // 배팅 모달에 관한 것들 =====================================================
+  const [balance,setBalance] = useState(0);
+  const [open, setOpen] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [goal, setGoal] = useState(0);
+  const [isWin, setIsWin] = useState(false);
 
-  // const handleEndTyping = (id: any) => {
-  //   setMessages((prevMessages) =>
-  //     // 이전 메세지들을 전부 순회하면서, 그 중 제일 최근 메세지의 ChatBot Animation 여부를 false로 바꾼다. (isTyping == 챗봇의 애니메이션 여부)
-  //     prevMessages.map((msg) =>
-  //       msg.id === id ? { ...msg, isTyping: false } : msg
-  //     )
-  //   );
 
-  // 타이핑이 종료되면, 더 이상 타이핑 중인 메세지가 없으므로 currentTypingId의 상태를 null 로 바꾼다.
+  // 배팅 함수==================================================
+  const onClickRadio = (value:boolean) => {
+    setIsWin(value);
+    console.log(value);
+  }
 
-  //   setCurrentTypingId(null);
-  // };
+
+  const { toast } = useToast();
+
+  // 배팅 제출 form 
+  function onSubmitBetting(data:any) {
+    
+    axios
+    .post(
+      `${import.meta.env.VITE_API_BASE_URL}/api/betting/${roomId}`,
+      {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        }
+      }, {
+        params: {
+          user_id: data.userId,
+          cur_betting_point: data.cur_betting_point,
+          is_win:isWin
+        }
+      }
+    )
+    .then((res) => {
+
+      setBalance(balance - res.data.data.myBettingPoint);
+
+      toast({
+        title: "배팅이 성공적으로 이루어졌습니다만?",
+        description:`이긴다에 걸린 총 금액: ${res.data.data.predictDto.predictWin}, 진다에 걸린 총 금액: ${res.data.data.predictDto.predictLose}
+        ,잔액: ${balance}
+        `,
+      })
+
+
+      
+    const winOrLose = isWin? "이긴다" : "진다.";
+
+    var chatMessage = {
+      chatRoomId: roomId2,
+      userId: userInfo.userId,
+      content: `${userInfo.userName}님이 현재 ${winOrLose}에 ${res.data.data.myBettingPoint} point 거셨습니다!`,
+      messageType: "CHAT_BOT",
+      imgCode: chatImage,
+    };
+    stompClient.send("/pub/chat/sendMessage", {}, JSON.stringify(chatMessage));
+
+    })
+    .catch((error)=> {
+      console.log(error);
+
+      toast({
+        variant: "destructive",
+        title: "현재 10분이 지나서 배팅할 수 있는 시간이 아닙니다!",
+        description: "배팅 불가!!!",
+      })
+
+    })
+
+    setOpen(false);
+
+  }
+
+  // ================================================================
 
   //currentTypingId를 최신화 한다.
   useEffect(() => {
@@ -189,24 +271,12 @@ export default function LiveChat() {
       userProfileImg: chat.userProfileImg,
     };
 
-    /*
-         // 내가 쓴 메세지
-      { text: chat.content, isUser: true },
-
-      // ChatBot이 쓴 메세지 
-      {
-        text: `당신의 메세지는: "${chat.content}"`,
-        isUser: false,
-        // 타이핑 애니메이션을 내는 트리거 
-        isTyping: true,
-        id: Date.now()
-      }
-    */
 
     // 소켓에서 받은 메세지를 전체 배열에 넣는 걸로 바꿔야함.
     // 이전 메세지를 받아서 메세지 전체 배열에 저장
     setMessages((prevMessages) => [...prevMessages, messageDTO]);
   }
+
 
   useEffect(() => {
     connect();
@@ -233,8 +303,6 @@ export default function LiveChat() {
           ...prevMessages,
           ...res.data.data.content.reverse(),
         ]);
-        // console.log("ddd");
-        // console.log(res.data.data.content);
       });
 
     axios
@@ -253,6 +321,36 @@ export default function LiveChat() {
         setDogamList(res.data.data);
       });
 
+    axios
+    .get(
+      `${import.meta.env.VITE_API_BASE_URL}/api/betting/room/${roomId}`,
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    )
+    .then((res:any) => {
+      setPredictObject(res.data.data);
+    })
+
+    axios
+    .get(      
+    `${import.meta.env.VITE_API_BASE_URL}/api/betting/user/${userInfo.userId}`,
+    {
+      headers: {
+        Authorization: token,
+      },
+    })
+    .then(
+      (res:any) => {
+       setBalance(res.data.data);
+      }
+    )
+
+    const timer = setTimeout(() => setProgress((predictObject.predictLose+predictObject.predictWin) === 0?
+    (0) : (predictObject.predictWin/(predictObject.predictLose + predictObject.predictWin)) * 100), 500)
+
     return () => {
       stompClient.send(
         "/pub/chat/exitChatRoom",
@@ -266,13 +364,12 @@ export default function LiveChat() {
       );
       stompClient
         .disconnect
-        // function () {
-        //   alert("see you next Time!!");
-        // }
-        // { userId: userInfo.userId, chatRoomId: userInfo.roomId },
-        // { userId: userInfo.userId, chatRoomId: userInfo.roomId }
         ();
+        clearTimeout(timer)  
     };
+
+
+
   }, [roomId]);
 
   const [chatImage, setChatImage]: any = useState(null);
@@ -310,32 +407,85 @@ export default function LiveChat() {
     });
   };
 
+  const onClickDrawer = (adjustment: number) => {
+    setGoal(Math.max(200, Math.min(1000, goal + adjustment)))
+  }
+
+  const data = [
+    {
+      color: "hsl(var(--foreground))",
+      goal: 400,
+    },
+    {
+      color: "hsl(var(--foreground))",
+      goal: 300,
+    },
+    {
+      color: "hsl(var(--foreground))",
+      goal: 200,
+    },
+    {
+      color: "hsl(var(--foreground))",
+      goal: 300,
+    },
+    {
+      name: "친구들",
+      color: "hsl(var(--foreground))",
+      goal: 200,
+    },
+    {
+      color: "hsl(var(--foreground))",
+      goal: 278,
+    },
+    {
+      color: "hsl(var(--foreground))",
+      goal: 189,
+    },
+    {
+      color: "hsl(var(--foreground))",
+      goal: 239,
+    },
+    {
+      color: "hsl(var(--foreground))",
+      goal: 300,
+    },
+    {
+      color: "hsl(var(--foreground))",
+      goal: 200,
+    },
+    {
+      name : "당신",
+      color: "rgba(234,19,24,1)",
+      goal: goal,
+    },
+  ]
+
+
   return (
     <>
       {/* ------------------------------------------------------- */}
       {/* 피시 화면 */}
-      <div className="w-full h-screen flex flex-col items-center mb-4 pt-10">
-        <div className="w-[700px] h-full border-4 rounded-xl grid grid-rows-12">
-          {/* 채팅 화면 상단 사용자 정보 바 */}
-          <div className="w-full row-span-2 flex justify-between items-center rounded-3xl border-b-4">
-            {/* 왼쪽 사용자 정보 */}
-            <div className="h-full w-[300px] flex items-center justify-center gap-8 ml-10">
-              {/* 사용자 프사 */}
-              <img
-                src={friendsInfo.playerProfilePic}
-                className=" rounded-full w-[80px] h-[80px]"
-              />
-              <div className="flex flex-col items-center gap-3">
-                {/* 칭호 */}
-                <MessageAlias alias={friendsInfo.playerAlias} />
-                {/* 이름 */}
-                <p className="font-bold text-2xl">{friendsInfo.playerName}</p>
-              </div>
-            </div>
-            {/* 프로필 보기 버튼 */}
-            {/* 해당 사람의 프로필로 이동한다 */}
-            {/* 임시 정적 데이터로 profileId를 넣었음 */}
-            <Link to={`/main/profile/${friendsInfo.playerId}`}>
+
+      {/* 배팅 현황 화면 */}
+      <Alert className=" w-[700px] h-[100%] m-auto" >
+      <div className="flex flex-row justify-between  h-full">
+        <div className="flex flex-row">
+                  {/* 프사 */}
+        <img
+            src={friendsInfo.playerProfilePic}
+            className=" rounded-full w-[80px] h-[80px] mb-3"
+                />
+        <div className="flex flex-col items-center gap-3 ml-3">
+        {/* 칭호 */}
+          <MessageAlias alias={friendsInfo.playerAlias} />
+        {/* 이름 */}
+          <div className="flex flex-row">
+            <p className="font-bold text-2xl">{friendsInfo.playerName}님이 이긴다!! : { (predictObject.predictLose+predictObject.predictWin) === 0?
+          (0) : (predictObject.predictWin/(predictObject.predictLose + predictObject.predictWin)) * 100} % </p> 
+          </div>
+        </div> 
+        </div>
+        <Link to={`/main/profile/${friendsInfo.playerId}`}>
               <Button
                 type="submit"
                 variant="secondary"
@@ -343,8 +493,96 @@ export default function LiveChat() {
               >
                 프로필 보기
               </Button>
-            </Link>
-          </div>
+            </Link>   
+      </div>    
+        <AlertTitle>   
+        <Progress style={{width: "100%", height: "50px", alignSelf:"center"}} value={progress} />
+        </AlertTitle>
+        <AlertDescription className="flex flex-row justify-between">
+          <Drawer open={open} onOpenChange={setOpen}>
+            <DrawerTrigger asChild>
+              <Button variant="outline">배팅하러 가기</Button>
+            </DrawerTrigger>
+            <DrawerContent>
+              <div className="mx-auto w-full max-w-sm">
+                <DrawerHeader>
+                  <DrawerTitle>얼마 걸 것이여?</DrawerTitle>
+                  <DrawerDescription>당신의 현재 잔액: {balance} point <br/> (기본 배팅 단위는 200 point 입니다.)</DrawerDescription>
+                </DrawerHeader>
+                <div className="p-4 pb-0">
+                <RadioGroup defaultValue="true" className="flex flex-row justify-around">
+                      <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="true" id="r2" onClick={() => onClickRadio(true)} />
+                        <Label htmlFor="r2">이긴다</Label>
+                      </div>
+                      <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="false" id="r3" onClick={() => onClickRadio(false)}  />
+                        <Label htmlFor="r3">진다</Label>
+                      </div>
+                  </RadioGroup>
+
+                  <div className="flex items-center justify-center space-x-2">
+
+                    <Button
+                      variant="outline"
+                      size="icon"
+                      className="h-8 w-8 shrink-0 rounded-full"
+                      onClick={() => onClickDrawer(-200)}
+                      disabled={goal <= 200}
+                    >
+                      <MinusIcon className="h-4 w-4" />
+                      <span className="sr-only">Decrease</span>
+                    </Button>
+                    <div className="flex-1 text-center">
+                      <div className="text-7xl font-bold tracking-tighter">
+                        {goal}
+                      </div>
+                      <div className="text-[0.70rem] uppercase text-muted-foreground">
+                        POINTS
+                      </div>
+                    </div>
+                    <Button
+                      variant="outline"
+                      size="icon"
+                      className="h-8 w-8 shrink-0 rounded-full"
+                      onClick={() => onClickDrawer(200)}
+                      disabled={goal >= 1000}
+                    >
+                      <PlusIcon className="h-4 w-4" />
+                      <span className="sr-only">Increase</span>
+                    </Button>
+                  </div>
+                  <div className="mt-3 h-[120px]">
+                    <ResponsiveContainer width="100%" height="100%">
+                      <BarChart data={data}>
+                      <XAxis dataKey="name" />
+                      <Bar
+                          dataKey="goal"
+                        >
+                        {data.map((entry,index) => (
+                          <Cell key={`cell-${index}`} fill={entry.color} />
+                        ))}  
+                        </Bar>
+                      </BarChart>
+                    </ResponsiveContainer>
+                  </div>
+                  
+                </div>
+                <DrawerFooter>
+                  <Button onClick={() => onSubmitBetting({userId: userInfo.userId,roomId: roomId, cur_betting_point: goal, is_win: isWin})}>배팅!</Button>
+                  <DrawerClose asChild>
+                    <Button variant="outline">그..그만둘래</Button>
+                  </DrawerClose>
+                </DrawerFooter>
+              </div>
+            </DrawerContent>
+        </Drawer>
+        </AlertDescription>
+      </Alert>
+      <Toaster/>    
+      <div className="w-full h-screen flex flex-col items-center mb-4 pt-10"> 
+        <div className="w-[700px] h-full border-4 rounded-xl grid grid-rows-12">
+          {/* 채팅 화면 상단 사용자 정보 바 */}
           {/* -------------------------------------------------------------------------------------------------------------------- */}
 
           {/* 채팅창 부분 */}
@@ -385,7 +623,7 @@ export default function LiveChat() {
           {/* -------------------------------------------------------------------------------------------------------------------- */}
 
           {/* 채팅 하단 부분 */}
-          <div className="flex border-b-4 border-blue-300 w-full row-span-1 justify-center items-center gap-1 rounded-3xl">
+          <div className="flex  border-b-4 border-blue-300 w-full row-span-1 justify-center items-center gap-1 rounded-3xl ">
             {/* 사진 버튼 */}
             <Dialog>
               <DialogTrigger asChild>
@@ -539,6 +777,7 @@ export default function LiveChat() {
               </button>
             </form>
           </div>
+          
         </div>
       </div>
 
