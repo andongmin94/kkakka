b497875e14101b39e7c0440f0c06f5849cd43429
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-02-13 17:24:07 +0900

    refactor: 일대일 채팅방 ui 수정

--- frontend/src/components/message/SysMsg.tsx
+++ frontend/src/components/message/SysMsg.tsx
@@ -23,7 +23,7 @@ export default function SysMsg({ data }: { data: dataProps }) {
   //       });
   return (
     <>
-      <div className="max-w-[70%] m-2 p-3 rounded-2xl self-end bg-slate-400 text-white font-bold text-xl text-wrap">
+      <div className="max-w-[70%] m-2 pt-2 pb-1 px-2 self-end  text-gray-600 text-sm text-wrap border-b-2 border-b-slate-400">
         {/* 이미지인지 텍스트 채팅인지 판별 */}
         <TypeIt
           options={{
