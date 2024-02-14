9d40a969cf60b1acc894bb6b9a8098703de5b006
JIYEON KIM <125720796+jiyeon2536@users.noreply.github.com>
2024-02-14 15:56:52 +0900

    refactor : 프로필 도감 ui 수정

--- frontend/src/components/profile/Collection.tsx
+++ frontend/src/components/profile/Collection.tsx
@@ -1,6 +1,5 @@
 // Collection.tsx
 
-import ThumbsDown from "@/components/profile/ThumbsDown";
 import { ProfileDogamWithDislikeNumType } from "@/types/dogamTypes";
 import { Link } from "react-router-dom";
 
@@ -10,47 +9,19 @@ export default function Collection({
   profiledogam: ProfileDogamWithDislikeNumType;
 }) {
   return (
-    <div className="flex-initial md:w-auto mb-3 mr-3">
-      <div
-        className="h-[170px] border-2 border-inherit mt-2 rounded-3xl  rounded-b-none border-b-0"
-        style={{
-          backgroundImage: `url(${profiledogam.dogamImgUrl})`,
-          backgroundSize: "cover",
-        }}
-      />
-      <div className="border-2 border-inherit rounded-3xl bg-cover rounded-t-none border-t-0 mt-0 ">
-        {/* 제목 */}
-        <Link
-          to={`/main/dogam/${profiledogam.dogamId}`}
-          className="w-full font-bold text-md px-4"
-        >
-          {profiledogam.dogamTitle}
-        </Link>
-        <div className="flex pb-3 font-bold items-center w-full">
-          {/* 싫어요 */}
-          <div className="lg:hover:scale-110 transition-transform ease-in-out duration-500">
-            <ThumbsDown
-              tD={profiledogam.hated}
-              dogamId={profiledogam.dogamId}
-              dogamDislikeNum={profiledogam.dogamDislikeNum}
-              setDogamDislikeNum={profiledogam.setDogamDislikeNum}
-            />
-          </div>
-          <div className="flex self-center text-sm text-blue-600">
-            {profiledogam.dogamDislikeNum}
-          </div>
-          {/* 댓글 */}
-          <Link to={`/main/dogam/${profiledogam.dogamId}`} className="text-xl">
-            💬
-          </Link>
-          {/* 등록일 */}
-          <div className="font-bold w-1/2">
-            <div className="text-end">
-              {profiledogam.createdAt.slice(0, 10)}
-            </div>
+    <div className="flex flex-col justify-center items-center shadow-lg  bg-gray-400 rounded-md mr-8 mb-8 relative overflow-hidden mt-3">
+      <Link to={`/main/dogam/${profiledogam.dogamId}`}>
+        <img src={profiledogam.dogamImgUrl} />
+        {/* 오버레이 */}
+        <div className="absolute bg-black top-0 left-0 bg-opacity-50 opacity-0 hover:opacity-100 rounded-md w-[100%] h-[100%] flex justify-center items-center">
+          {/* 타이틀 */}
+          <div className=" text-lg text-center mb-3 w-[50%] text-white drop-shadow-md ">
+            {profiledogam.dogamTitle.length > 30
+              ? `${profiledogam.dogamTitle.substring(0, 30)}...`
+              : profiledogam.dogamTitle}
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
