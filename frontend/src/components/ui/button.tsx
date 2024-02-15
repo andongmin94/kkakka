ad655abfccbac0585122a02193b148aa41e55279
JIYEON KIM <jiyeon2536@naver.com>
2024-02-15 11:07:30 +0900

    refactor : 버튼 전체 색 변경
    Details:
    - Commit: 1da60611d3fd377e624aa6c1e251ce7d6d457824
    - Author: JIYEON KIM <jiyeon2536@naver.com>
    - Date: 2024-02-15
    - Parent count: 1
    - Line changes: +23 / -15
    - Files:
    - M: frontend/src/components/itemShop/Compliment.tsx
    - M: frontend/src/components/itemShop/Speaker.tsx
    - M: frontend/src/components/itemShop/WriteAlias.tsx
    - M: frontend/src/components/navbar/Alarm.tsx
    - M: frontend/src/components/ui/button.tsx
    - M: frontend/src/routes/FirstLoginPage.tsx
    - M: frontend/src/routes/LiveChat.tsx
    - M: frontend/src/routes/LoginSuccessPage.tsx

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-blue-600",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground hover:bg-red-600",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
