98266852c11faae7bdee0414228671a53547afea
JIYEON KIM <jiyeon2536@naver.com>
2024-02-15 10:50:39 +0900

    feat : 채팅방 맨 아래로 이동 버튼 생성
    Details:
    - Commit: 406eae873db86c77cbac310a133c85c66f78da43
    - Author: JIYEON KIM <jiyeon2536@naver.com>
    - Date: 2024-02-15
    - Parent count: 1
    - Line changes: +30 / -2
    - Files:
    - M: frontend/src/components/message/BotMsg.tsx
    - M: frontend/src/routes/LiveChat.tsx
    - M: frontend/src/routes/MessageTestPage.tsx

--- frontend/src/components/message/BotMsg.tsx
+++ frontend/src/components/message/BotMsg.tsx
@@ -13,7 +13,7 @@ interface dataProps {
 export default function BotMsg({ data }: { data: dataProps }) {
   return (
     <>
-      <div className="w-[100%] my-1 py-2 self-end  bg-cyan-700 bg-opacity-70 drop-shadow-sm text-white text-md text-wrap dark:text-black text-center">
+      <div className="w-[100%] mb-1 py-2 self-end  bg-gray-500 bg-opacity-70 drop-shadow-sm text-white text-md text-wrap dark:text-black text-center">
         {/* 이미지인지 텍스트 채팅인지 판별 */}
         <TypeIt
           options={{
