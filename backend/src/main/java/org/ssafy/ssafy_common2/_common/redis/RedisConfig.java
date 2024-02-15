e91ee2ba8605a51a68d1a93e7f6e3c8fb71b5335
오세영 <osy9536@naver.com>
2024-02-15 12:24:18 +0900

    Feat : S10P12D110-313 redis 설정
    redisLockRepo 작성, Transaction 관리를 위한 Handler 작성, redis 사용을 위한 config 작성
    
    S10P12D110-313 #done #comment 닫기

---
+++ backend/src/main/java/org/ssafy/ssafy_common2/_common/redis/RedisConfig.java
@@ -0,0 +1,39 @@
+package org.ssafy.ssafy_common2._common.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
+    @Bean
+    public RedisConnectionFactory lettuceConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setPassword(password);
+
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory());
+        return redisTemplate;
+    }
+}
