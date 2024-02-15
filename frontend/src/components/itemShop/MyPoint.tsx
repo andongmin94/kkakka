884ddc30e0905b0995bf836f233ef9dea2cf6311
JIYEON KIM <jiyeon2536@naver.com>
2024-02-15 17:46:18 +0900

    fix : 포인트 안 깎이는 에러 수정

--- frontend/src/components/itemShop/MyPoint.tsx
+++ frontend/src/components/itemShop/MyPoint.tsx
@@ -1,32 +1,12 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
-export default function MyPoint() {
-  const [point, setPoint] = useState<number>(0);
-
-  useEffect(() => {
-    if (!point) {
-      axios
-        .get(`${import.meta.env.VITE_API_BASE_URL}/api/users/point`, {
-          headers: {
-            Authorization: localStorage.getItem("token"),
-          },
-        })
-        .then((res) => {
-          console.log("포인트", res.data.data.Point);
-          setPoint(res.data.data.Point);
-        });
-    }
-  }, [point]);
-
+export default function MyPoint({ point }: { point: number }) {
   return (
-    <div className="flex justify-between mr-0 mb-6">
+    <div className="flex justify-between mr-0 mb-6 ">
       <div></div>
-      <div className="flex bg-slate-300 rounded-md  bg-opacity-50 px-6 py-2 items-center">
+      <div className="flex bg-slate-300 rounded-md  bg-opacity-50 px-4 py-2 items-center shadow-md">
         <div className="mr-3">💰</div>
-        <div className="flex flex-col">
-          <div className="mr-3">내 포인트</div>
-          <div>{point}</div>
+        <div className="flex ">
+          <div className="mr-1">내 포인트</div>
+          <div>{point} P</div>
         </div>
       </div>
     </div>
